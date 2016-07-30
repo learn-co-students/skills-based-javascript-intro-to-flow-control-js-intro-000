355c5210138940b07771b1246ba837bc7b4e4f30
Matthew Hallquist <matthallquist@yahoo.com>
2016-07-30 19:28:05 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,21 +1,46 @@
 'use strict';
 
 function basicTeenager(age){
-
+if (age > 12 && age < 20) {
+  return "You are a teenager!"
+}
 }
 
 function teenager(age){
-
+if (age > 12 && age < 20) {
+  return "You are a teenager!"
+}
+else {
+  return "You are not a teenager"
+}
 }
 
 function ageChecker(age){
-
+if (age > 12 && age < 20) {
+  return "You are a teenager!"
+}
+else if (age <= 12) {
+  return "You are a kid"
+}
+else if (age >= 20) {
+  return "You are a grownup"
+}
 }
 
 function ternaryTeenager(age){
-
+if (age > 12 && age < 20) {
+  return "You are a teenager"
+}
+else {
+  return "You are not a teenager"
+}
 }
 
 function switchAge(age){
-
+if (age > 12 && age < 20) {
+  return "You are a teenager"
+}
+else {
+  return "You have an age"
+}
 }
