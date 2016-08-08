6384b3529cf701c0fd4b90bce23d89719736f3d0
David Ganthier <david.ganthier@gmail.com>
2016-08-08 23:46:31 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -12,6 +12,49 @@ function basicTeenager(age){
   }
 }
 
+-------------------------------------------------------------
+[2]
+
+function basicTeenager(age) {
+  if (age >= 13) {
+    console.log("You are a teenager!")
+  }
+  else if (ape <= 19) {
+    console.log("You are a teenager")
+  }
+  else (age = false) {
+    console.log("You are not a teenager.")
+  }
+
+}
+
+-------------------------------------------------------------
+[3]
+
+function basicTeenager(age) {
+  if (age >= 13) {
+    return true("You are a teenager");
+  }
+  else if (ape <= 19) {
+    return true("You are a teenager");
+  }
+  else (age = false) {
+    return false("You are not a teenager.")
+  }
+}
+-------------------------------------------------------------
+[4]
+
+function basicTeenager(age) {
+  if (age >= 13, age <= 19 ) {
+    return true("You are a teenager");
+  }
+  else (age = false) {
+    return false("You are not a teenager.")
+  }
+}
+-------------------------------------------------------------
+
 function teenager(age){
 
 }
