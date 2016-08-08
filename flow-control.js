eed83f1a9bf77ae1782d639a29f9ff6bbde9b615
syed shaheeruddin <shaheeruddinsyeds@gmail.com>
2016-08-08 16:37:29 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -26,7 +26,7 @@ else{
 }
 
 function ternaryTeenager(age){
-console.log(age>=13 && age<=19 ? "You are a teenager" : "You are not a teenager")
+return age>=13 && age<=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age){
