76bca1a57636856b70531abfcce0bcd8d813ab7f
David Ganthier <david.ganthier@gmail.com>
2016-08-09 04:18:54 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -32,7 +32,7 @@ function ageChecker(age){
 }
 
 function ternaryTeenager(age){
-  age >= 13, age <= 19? "You are a teenager" : "You are not a teenager";
+  age >= 13 && age <= 19? "You are a teenager" : "You are not a teenager";
 
 }
 
