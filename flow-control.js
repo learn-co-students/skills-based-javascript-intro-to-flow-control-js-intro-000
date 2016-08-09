884da4f5fa25767c3dfd00d63f14c04e7338e5b9
CBristow26 <cbristow26@gmail.com>
2016-08-09 17:10:26 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,30 @@
 'use strict';
+function basicTeenager(age) {
+    if (age >= 13 && age <= 19)
+    return "You are a teenager!"{}}
 
-function basicTeenager(age){
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function teenager(age){
 
