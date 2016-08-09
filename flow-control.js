334adb6224ba3483fd563d07e7832746e8f99b14
David Ganthier <david.ganthier@gmail.com>
2016-08-09 00:22:17 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,74 +1,13 @@
 'use strict';
 
 function basicTeenager(age) {
-  if (age >= 13) {
-    console.log("You are a teenager!")
-  }
-  else if (ape <= 19) {
+  if (age >= 13, age <= 19 ) {
     console.log("You are a teenager!")
   }
   else (age) {
     console.log("You are not a teenager.")
   }
-
 }
-
--------------------------------------------------------------
-[1]
-
-function basicTeenager(age) {
-  if (age >= 13 && <= 19) {
-    text = "You are a teenager!"
-  }
-  else if (age <= 12 && >= 20) {
-    text = "You are not a teenager."
-  }
-  else {
-    text = "You did not enter a numerical value."
-  }
-}
--------------------------------------------------------------
-[2]
-
-function basicTeenager(age) {
-  if (age >= 13) {
-    console.log("You are a teenager!")
-  }
-  else if (ape <= 19) {
-    console.log("You are a teenager")
-  }
-  else (age = false) {
-    console.log("You are not a teenager.")
-  }
-
-}
-
--------------------------------------------------------------
-[3]
-
-function basicTeenager(age) {
-  if (age >= 13) {
-    return true("You are a teenager");
-  }
-  else if (ape <= 19) {
-    return true("You are a teenager");
-  }
-  else (age = false) {
-    return false("You are not a teenager.")
-  }
-}
--------------------------------------------------------------
-[4]
-
-function basicTeenager(age) {
-  if (age >= 13, age <= 19 ) {
-    return true("You are a teenager");
-  }
-  else (age = false) {
-    return false("You are not a teenager.")
-  }
-}
-------------------------------------------------------------
 
 function teenager(age){
 
