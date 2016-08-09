f4e440300c5690bfa93e0b9e1923ce43952c03b3
David Ganthier <david.ganthier@gmail.com>
2016-08-09 01:32:08 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,16 +1,21 @@
 'use strict';
 
-function basicTeenager(age) {
-  if (age >= 13, age <= 19) {
+function basicTeenager(age){
+  if (age >= 13, age <= 19){
     return("You are a teenager!")
   }
-  else if (age) {
+  else if (age){
     return("You are not a teenager.")
   }
 }
 
 function teenager(age){
-
+  if (age >= 13, age <=19){
+    return("You are a teenager!")
+  }
+  else if (age){
+    return("You are not a teenager")
+  }
 }
 
 function ageChecker(age){
