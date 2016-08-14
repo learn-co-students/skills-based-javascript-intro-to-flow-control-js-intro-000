17ed45fee968fa2288ba1af20eb0eec3c8d9446a
Victoria Abanilla <myvitriol@gmail.com>
2016-08-14 02:06:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -2,14 +2,14 @@
 
 function basicTeenager(age){
   if (age >= 13 && age <= 19)
-  return "You are a teenager!"
+  return `You are a teenager!`
 }
 
 function teenager(age){
   if (age >=13 && age <= 19)
-  return "You are a teenager!"
+  return `You are a teenager!`
 } else {
-  return "You are not a teenager"
+  return `You are not a teenager`
 }
 
 function ageChecker(age){
