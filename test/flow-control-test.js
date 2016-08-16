d3e4da3fe0350cd35058c40a55298262ffd642ac
Carrie Jeanne <cdonica@ufl.edu>
2016-08-16 21:01:38 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -5,16 +5,12 @@ const fs = require('fs')
 const jsdom = require('mocha-jsdom')
 const path = require('path')
 
-describe('flow-control', () => {
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'flow-control.js'), 'utf-8')
-  })
 
 
 
 function basicTeenager(age) {
   if (age >= 13 && age <= 19) {
-  return "You are a teenager!"
+  return ("You are a teenager!");
  }
 }
 
@@ -22,19 +18,19 @@ function basicTeenager(age) {
 
 function teenager (age) {
   if (age > 12 && age <= 20) {
-  return "You are a teenager!"
+  return ("You are a teenager!");
 } else {
-  return "You are not a teenager"
+  return ("You are not a teenager");
 }
 }
 
 function ageChecker(age) {
    if (age < 20 && age > 12) {
-   return "You are a teenager"
+   return ("You are a teenager");
  } else if (age <=12) {
-   return "You are a kid"
+   return ("You are a kid");
  }  else {
-   return "You are a grownup"
+   return ("You are a grownup");
  }
  }
 
@@ -42,9 +38,9 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   if(age => 13 && age <= 19) {
-  return "You are a teenager"
+  return ("You are a teenager");
 } else {
-  return "You are not a teenager"
+  return ("You are not a teenager");
 }
 }
 
@@ -52,26 +48,27 @@ function ternaryTeenager(age) {
     function switchAge (age) {
       switch (age) {
       case 13:
-      return "You are a teenager"
+      return ("You are a teenager");
       break;
       case 14:
-      return "You are a teenager"
+      return ("You are a teenager");
       break;
       case 15:
-      return "You are a teenager"
+      return ("You are a teenager");
       break;
       case 16:
-      return "You are a teenager"
+     return ("You are a teenager");
       break;
       case 17:
-      return "You are a teenager"
+      return ("You are a teenager");
       break;
       case 18:
-      return "You are a teenager"
+      return ("You are a teenager");
       break;
       case 19:
-      return "You are a teenager"
+      return ("You are a teenager");
       break;
     default:
-    return "You are not a teenager"
-  }}
+    return ("You are not a teenager");
+}
+}
