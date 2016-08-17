f38003f774d9e5be8f592e12419fd90ec6655be2
Anina Köhler <anina.koehler@live.de>
2016-08-17 12:56:44 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -26,17 +26,25 @@ function ageChecker(age){
 }
 
 function ternaryTeenager(age){
-  if(age > 12 && age < 20){
-    return "You are a teenager"
-  } else{
-    return "You are not a teenager"
-  }
+  return (age > 12 && age < 20) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age){
-  if(age > 12 && age < 20){
-    return "You are a teenager"
-  } else{
-    return "You have an age"
+  switch (age){
+    case 13: return "You are a teenager"
+    break;
+    case 14: return "You are a teenager"
+    break;
+    case 15: return "You are a teenager"
+    break;
+    case 16: return "You are a teenager"
+    break;
+    case 17: return "You are a teenager"
+    break;
+    case 18: return "You are a teenager"
+    break;
+    case 19: return "You are a teenager"
+    break;
+    default: return "You have an age"
   }
 }
