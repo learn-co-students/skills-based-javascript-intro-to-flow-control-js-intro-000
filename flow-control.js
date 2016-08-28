9e864d9f8a9a1f047684016061a1e4f31b91c103
Kevin McCall <kmccall@Kevins-MacBook-Air.local>
2016-08-28 14:26:06 -0700

    begin flow-control lab

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,28 @@
 'use strict';
 
-function basicTeenager(age){
-
+function basicTeenager(age) {
+	if (age >= 13 && age <= 19) {
+		return 'You are a teenager!';
+	}
+	return undefined;
 }
 
 function teenager(age){
-
+	if (age >= 13 && age <= 19) {
+		return 'You are a teenager!';
+	}
+	return 'You are not a teenager!';
 }
 
 function ageChecker(age){
-
+	if (age >= 13 && age <= 19) {
+		return 'You are a teenager!';
+	} else if (age < 13) {
+		return "You are a kid";
+	} else if (age >= 20) {
+		return "You are a grownup";
+	}
+	return undefined;
 }
 
 function ternaryTeenager(age){
