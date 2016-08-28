325c23fd56943c192efcccb33039a76c76db18b7
Kevin McCall <kmccall@Kevins-MacBook-Air.local>
2016-08-28 14:36:59 -0700

    complete flow-control test

--- flow-control.js
+++ flow-control.js
@@ -11,7 +11,7 @@ function teenager(age){
 	if (age >= 13 && age <= 19) {
 		return 'You are a teenager!';
 	}
-	return 'You are not a teenager!';
+	return 'You are not a teenager';
 }
 
 function ageChecker(age){
@@ -26,9 +26,16 @@ function ageChecker(age){
 }
 
 function ternaryTeenager(age){
-
+	return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"; 
 }
 
 function switchAge(age){
-
+	switch (true) {
+		case (age >= 13 && age <= 19): {
+			return "You are a teenager";
+			}
+		default: {
+			return "You have an age";
+		}
+	}
 }
