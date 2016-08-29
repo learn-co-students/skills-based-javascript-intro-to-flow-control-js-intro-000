630441e617850a42157044194ebd395837452684
Fernando Gomez <fhergomez37@gmail.com>
2016-08-29 08:27:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,21 +1,43 @@
 'use strict';
 
 function basicTeenager(age){
-
-}
+  if ((age >= 13) && (age <= 19)){
+    return 'You are a teenager!';
+  } else {
+    return undefined;
+  };
+};
 
 function teenager(age){
-
-}
+  if ((age < 13) || (age > 19)){
+    return 'You are not a teenager';
+  } else {
+    return 'You are a teenager!';
+  }
+};
 
 function ageChecker(age){
-
-}
+  if((age >= 13) && (age <= 19)){
+    return 'You are a teenager!';
+  } else if (age <= 12){
+    return 'You are a kid';
+  } else {
+    return 'You are a grownup';
+  }
+};
 
 function ternaryTeenager(age){
-
-}
+  if((age >= 13) && (age <= 19)){
+    return 'You are a teenager';
+  } else {
+    return 'You are not a teenager';
+  }
+};
 
 function switchAge(age){
-
-}
+  if(age >= 13 && age <= 19){
+    return 'You are a teenager';
+  } else {
+    return 'You have an age';
+  }
+};
