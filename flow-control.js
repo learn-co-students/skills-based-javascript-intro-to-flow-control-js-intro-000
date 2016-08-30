d6e27acb12eb75a68842f124e801066c4243f469
Galina Belyaeva <belyaeva.galya@gmail.com>
2016-08-30 01:03:30 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -36,17 +36,10 @@ function ternaryTeenager(age){
 function switchAge(age){
   switch(age) {
   case 13:
-        return "You are a teenager";
   case 14:
-        return "You are a teenager";
   case 15:
-        return "You are a teenager";
   case 16:
-        return "You are a teenager";
   case 17:
-        return "You are a teenager";
-  case 18:
-        return "You are a teenager";
   case 19:
         return "You are a teenager";
   default:
