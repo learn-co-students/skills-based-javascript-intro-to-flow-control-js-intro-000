6d890014c38a3347849c396a82f0c3d6ec46e265
Galina Belyaeva <belyaeva.galya@gmail.com>
2016-08-30 00:58:06 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,21 +1,57 @@
 'use strict';
 
 function basicTeenager(age){
-
-}
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager!"
+  }
+  }
 
 function teenager(age){
-
+  if (age >= 13 && age <= 19) {
+      return "You are a teenager!"
+  }
+  else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age){
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+}
+  else if (age <= 12) {
+    return "You are a kid"
+  }
+  else if (age > 19)
+    return "You are a grownup"
 }
 
 function ternaryTeenager(age){
-
+  var teenagerlife;
+  teenagerlife = (age >= 13 && age <= 19) ?
+  "You are a teenager" : "You are not a teenager";
+  return teenagerlife;
 }
 
 function switchAge(age){
-
+  var age;
+  switch(age) {
+  case 13:
+        return "You are a teenager";
+  case 14:
+        return "You are a teenager";
+  case 15:
+        return "You are a teenager";
+  case 16:
+        return "You are a teenager";
+  case 17:
+        return "You are a teenager";
+  case 18:
+        return "You are a teenager";
+  case 19:
+        return "You are a teenager";
+  default:
+        return "You have an age";
+}
+  return age;
 }
