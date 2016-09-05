1dc98ebee411683c0eb803a8f86b7238c67af4d8
Carlos Sosa <crsaapp@gmail.com>
2016-09-05 21:19:40 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -26,7 +26,8 @@ if(age>=13 && age <=19)
 }
 
 function ternaryTeenager(age) {
-  (age >=13 && age <=19)? "You are a teenager":"You are not a teenager";
+  return age >=13 && age <=19? 'You are a teenager':'You are not a teenager';
+
 
 }
 
