c6ce5e7bc68b1596d98efe527d2224210b3167dd
Tony <tony@refactoru.com>
2016-09-07 11:16:20 -0400

    Change capitalization of teenager

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -37,7 +37,7 @@ describe('flow-control', () => {
       expect(teenager(13)).toEqual("You are a teenager!")
     })
 
-    it('should return "you are not a teenager" if the age is not between 13-19', () => {
+    it('should return "You are not a teenager" if the age is not between 13-19', () => {
       expect(teenager(12)).toEqual("You are not a teenager")
       expect(teenager(29)).toEqual("You are not a teenager")
     })
