df7f30a40a74bc405aef20a1b8c8074730165203
Tony <tony@refactoru.com>
2016-09-07 11:18:32 -0400

    You have an age
    change test message to fit wording

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -72,7 +72,7 @@ describe('flow-control', () => {
       expect(switchAge(15)).toEqual("You are a teenager")
     })
 
-    it('should return "You are not a teenager" if age not between 13-19', () => {
+    it('should return "You have an age" if age not between 13-19', () => {
       expect(switchAge(75)).toEqual("You have an age")
       expect(switchAge(7)).toEqual("You have an age")
     })
