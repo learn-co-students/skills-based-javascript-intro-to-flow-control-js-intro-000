19c63a7d4a3854ce36114a2fc55163269d4743d9
Menaka Sampath <menakasampath@gmail.com>
2016-09-09 20:46:18 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -21,8 +21,9 @@ function ageChecker(age) {
   }}
 
 function ternaryTeenager(age) {
-    13 <=age && 19 >=age ? return "You are a teenager" : return "You are not a teenager"
+   return age > 12 && age < 20 ? "You are a teenager": "You are not a teenager"
 }
+
 
 function switchAge(age) {
   switch(age) {
