58a6bcd4e6938514e975ab3f6e968a3dd67280f6
p sc <patty.stacruz@gmail.com>
2016-09-22 20:17:10 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -28,19 +28,27 @@ function ternaryTeenager(age) {
   return (age > 12 && age < 20) ? "You are a teenager" : "You are not a teenager"
 }
 
-// function switchAge(age) {
-//   switch(age){
-//     case(age > 12 && age < 20):
-//       return "You are a teenager"
-//       break;
-//     default:
-//       return "You have an age"
-//   }
-// }
-function switchAge(age){
-  if (age > 12 && age < 20)
-    {return "You are a teenager"
-  }else {
-    return "You have an age"
+function switchAge(age) {
+  switch(true){
+    case(age > 12 && age < 20):
+      return "You are a teenager"
+      break;
+    default:
+      return "You have an age"
   }
 }
+//
+// function switchAge(age){
+//   switch (age) {
+//   case 13:
+//   case 14:
+//   case 15:
+//   case 16:
+//   case 17:
+//   case 18:
+//   case 19:
+//     return "You are a teenager";
+//   default:
+//     return "You have an age"
+//   }
+// }
