e8693e18d20d2b38fa2a5e634b723dc48f421638
Kartikawati Stephanus-Nolan <priscilla.kosasih@gmail.com>
2016-09-25 03:04:03 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -23,7 +23,7 @@ describe('flow-control', () => {
 
   describe('basicTeenager', () => {
     it('should return "You are a teenager!" if the age is between 13-19', () => {
-      expect(basicTeenager(13)).toEqual("You are a teenager!");
+      expect(basicTeenager(13)).toEqual("You are a teenager!")
     })
 
     it('should return undefined if the age is not between 13-19', () => {
