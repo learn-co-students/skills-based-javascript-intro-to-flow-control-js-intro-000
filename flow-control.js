93879a6b6c65f102f0c3c8a5cb2b936ad6a55039
Kevin Kreutz <kevin.parkkreutz@gmail.com>
2016-09-26 23:07:07 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -15,13 +15,14 @@ else if (age<13) {return "You are a kid"}
 else if (age>19) {return "You are a grownup"}
 };
 
-function ternaryTeenager(age) {(age >= 13 && age <= 19)?
-You are a teenager
-:
-You are not a teenager};
-/*
-function switchAge(age) {var(age)
-case "13,14,15,16,17,18,19"
-console.log{"You are a teenager"}
-default: {"You are an age"}};
-*/
+function ternaryTeenager(age) {return age >= 13 && age <= 19?
+"You are a teenager" : "You are not a teenager"};
+
+function switchAge(age) {
+switch(age) {
+case (13,14,15,16,17,18,19):
+return "You are a teenager";
+break;
+default:
+return "You have an age"}
+};
