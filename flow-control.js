9038c257cfdc6b1b7eaf6c06ef2e350b6ef088c7
Qianli Shi <charlies@live.com>
2016-10-06 02:47:52 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -27,9 +27,17 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  if (age <= 19 && age >= 13) {
-    return "You are a teenager"
-  } else {
-    return "You have an age"
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager"
+    default:
+      return "You have an age"
   }
+
 }
