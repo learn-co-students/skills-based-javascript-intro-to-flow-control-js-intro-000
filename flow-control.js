b2ace2a7100df3221093efc1339467ba4c405716
jason brown <j.grant.brown@gmail.com>
2016-10-11 18:51:05 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -9,13 +9,13 @@ if(age>=13 && age <=19){
 return "You are a teenager!";
 }
 else   {
-  return "You are not a teenager!";
+  return "You are not a teenager";
   }
 }
 
 function ageChecker(age) {
   if(age>=13 && age<=19){
-    return "You are a teenager!";
+    return "You are a teenager!";}
     else if (age<13){
       return "You are a kid";
     }
@@ -26,36 +26,21 @@ function ageChecker(age) {
 
 
 
-function ternaryTeenager(age) {
-  if age>=13 && age <=19)
-? "You are a teenager!":
-"You are not a teenager";
-}
+  function ternaryTeenager(age){
+    return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
+  }
 
-function switchAge(age) {
-switch(age){
-case 13:
-console.log("You are a teenager");
-break;
-case 14:
-console.log("You are a teenager");
-break;
-case 15:
-console.log("You are a teenager");
-break;
-case 16:
-console.log("You are a teenager");
-break;
-case 17:
-console.log("You are a teenager");
-break;
-case 18:
-console.log("You are a teenager");
-break;
-case 19:
-console.log("You are a teenager");
-break;
-default:
-console.log("You have an age");
-}
-}
+  function switchAge(age){
+    switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager";
+    default:
+      return "You have an age"
+    }
+  }
