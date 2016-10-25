70bf90e5205dea896c0cb7eea5733514aa332065
veechi tour <coreygames88@gmail.com>
2016-10-25 15:02:08 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,19 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
 
+if (age === 13, 14, 15, 16, 17, 18, 19){
+  console.log('You are a teenager!')
+} else { return 'You are not a teenager'
+}
 }
 
 function ageChecker(age) {
-
+if (age === 12){
 }
 
 function ternaryTeenager(age) {
