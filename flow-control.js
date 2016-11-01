83417ae5807df801433f27217d30500aa8f6b196
Amit Singh <amit_nyc@live.com>
2016-11-01 19:27:48 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,17 @@
-function if basicTeenager(13=>19) {
-  return basicTeenager(13=>19);
-  console.log("You are a teenager!")
-}
-basicTeenager(15)
+function basicTeenager(age){
+if (age >= 13 && age <= 19)
+{      console.log('"You are a teenager!"')
+}}
 
 
-function teenager(age) {
 
-}
+
+function teenager(age){
+  if (age >=13 && age <=19)
+{ console.log('"You are a teenager!"') }
+  else{ console.log('"You are not a teenager!"')
+}}
+
 
 function ageChecker(age) {
 
