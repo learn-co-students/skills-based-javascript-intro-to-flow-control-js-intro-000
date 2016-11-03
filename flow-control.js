c93819f740a87c1918ca5d8d53d169e3bf3c870d
Andrew Schubert <andrew.schubert1986@gmail.com>
2016-11-03 02:38:35 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,48 @@
 function basicTeenager(age) {
+ if (age > 12 && age < 20) {
+  return 'You are a teenager!'
+ }
+};
 
-}
 
 function teenager(age) {
-
+  if (age >12 && age <20) {
+    return 'You are a teenager!'
+  }
+  else {
+    return 'You are not a teenager'
 }
+};
+
 
 function ageChecker(age) {
+  if (age>12 && age <20) {
+    return 'You are a teenager!'
+  }
+  else if (age < 13) {
+    return 'You are a kid'
+  }
+  else {
+    return 'You are a grownup'
+  }
+};
 
-}
 
 function ternaryTeenager(age) {
+  if (age>12 && age<20) {
+    return 'You are a teenager'
+  }
+  else {
+    return 'You are not a teenager'
+  }
+};
 
-}
 
 function switchAge(age) {
-
-}
+  if (age<12 || age>20) {
+    return 'You have an age'
+  }
+  else {
+    return 'You are a teenager'
+  }
+};
