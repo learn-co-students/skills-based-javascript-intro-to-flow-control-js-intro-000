cda24a16b880d975200b6fda0ca4a1c2d18a63cd
Dave Leno <adfe33jafo@mailnesia.com>
2016-11-06 02:11:28 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,64 @@
-function basicTeenager(age) {
-
+function basicTeenager(age)
+{
+  if (age >= 13 && age <= 19)
+  {
+    return "You are a teenager!";
+}
 }
 
-function teenager(age) {
-
+function teenager(age)
+{
+  if (age >= 13 && age <= 19)
+  {
+    return "You are a teenager!";
+}
+  else
+  {
+    return "You are not a teenager";
+}
 }
 
-function ageChecker(age) {
-
+function ageChecker(age)
+{
+  if (age >= 13 && age <= 19)
+  {
+    return "You are a teenager!";
 }
-
-function ternaryTeenager(age) {
-
+  else if(age < 13)
+  {
+    return "You are a kid";
 }
-
-function switchAge(age) {
-
+  else if (age > 19)
+  {
+    return "You are a grownup";
+}
+}
+function ternaryTeenager(age)
+{
+  if (age >= 13 && age <= 19)
+  {
+   return "You are a teenager";
+ }
+  else
+  {
+    return "You are not a teenager";
+}
+}
+function switchAge(age)
+{
+  switch (age)
+  {
+case 13:
+case 14:
+case 15:
+case 16:
+case 17:
+case 18:
+case 19:
+    return "You are a teenager";
+    break;
+    default:
+    return "You have an age";
+    break;
+  }
 }
