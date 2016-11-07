7644f07c57c320153ee383e8bf43fb3007b7028d
Anna Brakowska <anna.brakowska91@gmail.com>
2016-11-07 01:02:23 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -3,7 +3,11 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if (age>=13 && age<=19) {
+    console.log ("You are a teenager")
+  } else {
+    return ("You are not a teenager")
+  }
 }
 
 function ageChecker(age) {
