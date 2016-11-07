da7af15a864f03bf5c2b160c1fc25075a768c8d1
Muhammad BIMONARETGA <bimonaretga@outlook.com>
2016-11-07 17:40:42 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -26,12 +26,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  if (age >= 13 && age <= 19){
-    return "You are a teenager"
-    }
-    else {
-      return "You are not a teenager"
-    }
+  (age >= 13 && age <= 19) ? return "You are a teenager" : return "You are not a teenager"
 }
 
 function switchAge(age) {
