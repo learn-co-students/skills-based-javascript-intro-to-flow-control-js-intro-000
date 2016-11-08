1054cc0146f51c347385723e4178fbe83059e3f7
Anna Brakowska <anna.brakowska91@gmail.com>
2016-11-08 02:17:42 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -27,5 +27,10 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case age<=13 && age>=19 :
+      return "You are a teenager"
+    default:
+      return "You have an age"
+    }
 }
