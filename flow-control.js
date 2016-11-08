acb4d3e61c3ef73ea14d9b01e7618b306be4ee02
Anna Brakowska <anna.brakowska91@gmail.com>
2016-11-08 02:59:47 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -6,10 +6,10 @@ function basicTeenager(age) {
 
 
 function teenager(age) {
-  if (age>=13 && age<=19) {
-    console.log ("You are a teenager")
+  if (age>=13) && (age<=19) {
+    return 'You are a teenager'
   } else {
-    return ("You are not a teenager")
+    return 'You are not a teenager'
   }
 }
 
@@ -35,20 +35,36 @@ function ternaryTeenager(age) {
 //     }
 // }
 
-function switchAge(age){
-  switch(age) {
-    case  13:
-    case  14:
-    case  15:
-    case  16:
-    case  17:
-    case  18:
-    case  19:
-      console.log("You are a teenager");
-    break;
-    default:
-      console.log("You have an age");
-  }
+// function switchAge(age){
+//   switch(age) {
+//     case  13:
+//     case  14:
+//     case  15:
+//     case  16:
+//     case  17:
+//     case  18:
+//     case  19:
+//       console.log("You are a teenager");
+//     break;
+//     default:
+//       console.log("You have an age");
+//   }
+// }
+
+
+function switchAge(age) {
+ switch(age) {
+   case 13:
+   case 14:
+   case 15:
+   case 16:
+   case 17:
+   case 18:
+   case 19:
+    return 'You are a teenager';
+   default:
+    return 'You have an age';
+ }
 }
 // functin switchAge(age){
 //
