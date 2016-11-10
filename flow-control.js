702d282f4b8249f160fda927ff87c29afa8cf6e8
Ilan Davidovics <ilanshamai@gmail.com>
2016-11-10 18:33:29 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,22 +1,21 @@
 function basicTeenager(age) {
-}
     if( (age > 13 && age < 19) ){
-      console.log("You are a teenager!");
+      return "You are a teenager!"
     }
 }
 
-//function teenager(age) {
+function teenager(age) {
 
-//}
+}
 
-//function ageChecker(age) {
+function ageChecker(age) {
 
-//}
+}
 
-//function ternaryTeenager(age) {
+function ternaryTeenager(age) {
 
-//}
+}
 
-//function switchAge(age) {
+function switchAge(age) {
 
-//}
+}
