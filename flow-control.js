882e4fda0133ce7914c979910879319a7196c0bf
Patrick Logan <jpatricklogan@gmail.com>
2016-11-18 15:07:48 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,56 @@
 function basicTeenager(age) {
-
+  if (age >=13 && age <= 19) {
+    return "You are a teenager!";
+  }
 }
+
 
 function teenager(age) {
-
-}
+  if (12 < age < 20)
+    return "You are a teenager"
+  }
+  else {
+    return "You are not a teenager"
+  }
 
 function ageChecker(age) {
-
-}
+  if (13<=age<=19)
+    return "You are a teenager!"
+  }
+  else if {
+    (age<13) return "You are a kid"
+  }
+  else return "You are a grownup"
+  }
 
 function ternaryTeenager(age) {
-
-}
+  12<age<20 ? "You are a teenager" : "You are not a teenager"
+  }
 
 function switchAge(age) {
-
+  switch(age) {
+    case 13:
+      console.log("You are a teenager");
+      break;
+    case 14:
+      console.log("You are a teenager");
+      break;
+    case 15:
+      console.log("You are a teenager");
+      break;
+    case 16:
+      console.log("You are a teenager");
+      break;
+    case 17:
+      console.log("You are a teenager");
+      break;
+    case 18:
+      console.log("You are a teenager");
+      break;
+    case 19:
+      console.log("You are a teenager");
+      break;
+    default:
+      return "You have an age"
+  }
 }
