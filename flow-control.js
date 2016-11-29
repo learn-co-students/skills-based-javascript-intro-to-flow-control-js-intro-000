c69038ba2c04936855558e77385624d95b83c2f2
Paul Snider <pbsnider@gmail.com>
2016-11-29 23:12:30 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -2,12 +2,14 @@ function basicTeenager(age) {
 if(age>=13 && age<=19) {
   return "You are a teenager!"
 }
-else {
-  return "You are not a teenager."
-}
 }
 
 function teenager(age) {
+  if(age>=13 && age<=19) {
+    return "You are a teenager!"
+  }
+  else {
+    return "You are not a teenager."
 
 }
 
