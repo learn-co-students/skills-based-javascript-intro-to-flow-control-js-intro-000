3ff328328cd61c5ae8af8a958e79af631778eade
Lily-Thérèse Walla <summerkid62@gmail.com>
2016-12-05 06:35:15 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -24,7 +24,13 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case age >= 13 && age <= 19:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+     case 19:
       return "You are a teenager";
     default:
     return "You have an age";
