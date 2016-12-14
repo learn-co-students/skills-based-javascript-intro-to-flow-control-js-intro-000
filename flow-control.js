d0d538cec009eaf93ad34f95750c0e37d6e4b167
Daniel Kweon-lee <dkweonlee@gmail.com>
2016-12-14 21:53:46 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,9 @@
 function basicTeenager(age) {
-if(13<=age<=19){
-  return 'You are a teenager!';
-}
-else {
-   return '';
-} 
 
 }
 
 function teenager(age) {
-if(age)
-return "You are a teenager!";
-}
-else {
-  return "You are not a teenager";
+
 }
 
 function ageChecker(age) {
