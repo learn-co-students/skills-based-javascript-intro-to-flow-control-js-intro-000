638ca1fb2b2c2602f72ae97ac7e151f57ac7ff73
Amy Ng <akitng@gmail.com>
2017-01-06 05:24:31 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,40 +1,59 @@
-'use strict';
-
-function basicTeenager(age){
-  if (age > 12 && age < 20){
-    return("You are a teenager!");
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
   }
-  return undefined;
 }
 
-function teenager(age){
-  if (age >=13 && age <= 19){
-      return("You are a teenager!");
-    }
+function teenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
   else {
-  return("You are not a teenager");
+    return "You are not a teenager";
+  }
 }
 
-};
-
-function ageChecker(age){
-  if (age >=13 && age <= 19){
-      return("You are a teenager!");
-    }
-  else if (age <= 12){
-    return("You are a kid");
+function ageChecker(age) {
+  if (age <= 12) {
+    return "You are a kid";
   }
   else if (age > 19) {
-    return ("You are a grownup")
+    return "You are a grownup";
   }
- else {
-   return;
- }
-};
-
-function ternaryTeenager(age){
-
-
-function switchAge(age){
-
+  else {
+    return "You are a teenager!";
+  }
 }
+
+function ternaryTeenager(age) {
+    return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager";
+}
+
+function switchAge(age) {
+  switch(age) {
+    case 13:
+      return "You are a teenager";
+      break;
+    case 14:
+      return "You are a teenager";
+      break;
+    case 15:
+      return "You are a teenager";
+      break;
+    case 16:
+      return "You are a teenager";
+      break;
+    case 17:
+      return "You are a teenager";
+      break;
+    case 18:
+      return "You are a teenager";
+      break;
+    case 19:
+      return "You are a teenager";
+      break;
+    default:
+      return "You have an age";
+  }
+}
+
