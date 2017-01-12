0778d2d5c58d6ccfd4e127437817108bec504e52
Freddie Hurtado <fhurt5@gmail.com>
2017-01-12 20:35:01 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,38 @@
 function basicTeenager(age) {
-
+if (age >= 13 && age <=19) {
+return("You are a teenager!")
+} else {
+  
+}
 }
 
 function teenager(age) {
-
-}
+  if (age >= 13 && age <=19) {
+  return("You are a teenager!")
+  } else {
+    return("You are not a teenager")
+  }
+  }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <=19) {
+  return("You are a teenager!")
+} else if (age < 13) {
+    return("You are a kid")
+  } else if (age >19) {
+    return("You are an adult")
+  }
 }
 
 function ternaryTeenager(age) {
-
+return age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+switch (age) {
+case (age>=13 && age <=19):
+return "You are a teenager"
+default:
+return "You have an age"
+}
 }
