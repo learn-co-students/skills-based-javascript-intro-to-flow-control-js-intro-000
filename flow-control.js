c4985a8cbbce252b6fc85ce4e8738dfe5d9f86e0
Jim Tryon <j.tryon@outlook.com>
2017-01-16 05:37:48 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,15 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
+  else if (age <= 12) {
+    return "You are a kid";
+  }
+  else if (age > 19) {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
