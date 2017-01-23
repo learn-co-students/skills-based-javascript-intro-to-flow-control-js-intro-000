3349c29001104607d02cbcdd94d6945abaf8b0f6
Walker Jorge Sousa de Souza <walker.souza@gmail.com>
2017-01-23 23:30:39 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -13,17 +13,17 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age <= 13 && age >= 19){
+  if (age >= 13 && age <= 19){
     return "You are a teenager!"
   } else if (age <=12){
     return "You are a kid"
   } else {
-    retun "You are a grownup"
+    return "You are a grownup"
   }
 }
 
 function ternaryTeenager(age) {
-  return (age >= 13 && age <= 19) ? "You are a teenager!" : "You are not a teenager"
+  return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
@@ -31,8 +31,8 @@ function switchAge(age) {
     case 13:
       return "You are a teenager";
     case 14:
-      return "You are a teenager";
     case 15:
+      return "You are a teenager";
       return "You are a teenager";
     case 16:
       return "You are a teenager";
