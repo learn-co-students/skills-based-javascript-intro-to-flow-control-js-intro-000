05276cf45391fc0056198af6d6a6e43fe76581f0
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-01-28 18:28:40 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -6,8 +6,11 @@ function basicTeenager(age) {
 
 
 function teenager(age) {
-
-
+  if (age >= 13 && age <= 19) {
+  return ('You are a teenager!')
+} else {
+  return ('You are not a teenager')
+}
 }
 
 function ageChecker(age) {
