9ec5d5dff6be4e416c0cafe1be7c7d3fddc5497d
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-01-28 20:24:26 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -26,7 +26,12 @@ if (age >= 13 && age <= 19) {
 }
 
 function ternaryTeenager(age) {
-
+if (age >= 13 && age <= 19) {
+  return ('You are a teenager')
+}
+ else {
+   return ('You are not a teenager')
+ }
 }
 
 function switchAge(age) {
