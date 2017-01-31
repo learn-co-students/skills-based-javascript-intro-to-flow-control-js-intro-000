d5484a9fb064c6be8a1e33b31dab05b8ab8a54a5
Alexandra Latimer <alexmlatimer@gmail.com>
2017-01-31 18:04:18 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -4,7 +4,6 @@ function basicTeenager(age) {
 }
 }
 
-
 function teenager(age) {
   if (age >13 && age <19) {
     return "You are a teenager!"
@@ -15,23 +14,20 @@ function teenager(age) {
 }
 }
 
-
 function ageChecker(age) {
   if (age >13 && age <19) {
     return "You are a teenager!"
   }
 }
-  else if { (age <=12) {
+  else if (age <=12) {
     return "You are a kid"
   }
-}
 else {
     return "You are a grownup"
-  }
 }
 
 function ternaryTeenager(age) {
-return age ===>13 && age <19? "You are a teenager" : "You are not a teenager"
+return age === >=13 && age <=19? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
