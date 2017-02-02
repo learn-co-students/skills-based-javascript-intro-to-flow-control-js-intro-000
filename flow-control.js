21b1ea623d34376384554d09acdf3e391e31ae3a
Chengpei Xu <mokakakaka@gmail.com>
2017-02-02 00:33:26 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,48 @@
 function basicTeenager(age) {
-
+  if (13 <=age && age<=19){
+    return("You are a teenager!")
+  }
 }
 
-function teenager(age) {
 
+function teenager(age) {
+  if (13 <= age && age <=19){
+    return("You are a teenager!")
+  }
+  else{
+    return("You are not a teenager")
+  }
 }
 
 function ageChecker(age) {
-
+  if (13 <=age && age <=19){
+    return("You are a teenager!")
+  }
+  if (age <=12){
+    return("You are a kid")
+  }
+  if (age >= 20){
+    return("You are a grownup")
+  }
 }
 
+
 function ternaryTeenager(age) {
+  if (13<=age && age <=19){
+    return("You are a teenager")
+  }
+  else {
+    return ("You are not a teenager")
+  }
 
 }
 
 function switchAge(age) {
+  if (13 <= age && age <=19){
+    return("You are a teenager")
+  }
+  else{
+    return("You have an age")
+  }
 
 }
