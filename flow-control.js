b63c8cd96788f8232d3ad229d8d93a27ba77d084
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-02-08 05:27:17 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,24 +1,24 @@
 function basicTeenager(age) {
   if (age > 12 && age < 20) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   }
 }
 
 function teenager(age) {
   if (age > 12 && age < 20) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else {
-    return "You are not a teenager"
+    return "You are not a teenager";
   }
 }
 
 function ageChecker(age) {
   if (age > 12 && age < 20) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else if (age <= 12) {
-      return "You are a kid"
+      return "You are a kid";
   } else if (age > 20) {
-      return "You are a grownup"
+      return "You are a grownup";
   }
 }
 
@@ -28,9 +28,15 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch (age) {
-    case age > 12 && age < 20):
-      return "You are a teenager"
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager";
     default:
       return "You have an age"
-  }
+    }
 }
