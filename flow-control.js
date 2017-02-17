21febabaec8c1bf4eb5f4003189d61f5964e9522
Renata Ramsini <renataramsini@gmail.com>
2017-02-17 22:40:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,13 @@
 function basicTeenager(age) {
-
+if (age >=13 || <=19)
+return "You are a teenager!"
+} else if (age <=13 || >=19) {
+return "You are not a teenager."
 }
 
 function teenager(age) {
+
+} else {
 
 }
 
