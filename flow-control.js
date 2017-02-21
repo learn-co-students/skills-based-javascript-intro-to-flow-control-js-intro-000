cd3857006b4fc18239801be3f034685b0d121ede
Jennifer Tam <jennifer.k.tam@gmail.com>
2017-02-21 21:36:55 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,18 +1,32 @@
 function basicTeenager(age) {
-
+  if(age >= 13 && age <= 19) {
+    console.log("You are a basic teenager!");
+  }
 }
 
 function teenager(age) {
-
+  if(age >= 13 && age <= 19) {
+    console.log("You are a teenager!");
+  }
+  else {
+    console.log("You are not a teenager");
+  }
 }
 
 function ageChecker(age) {
-
+  if(age >= 13 && age <= 19) {
+    console.log("You are a teenager!");
+  }
+  else if(age <=12 ){
+    console.log("You are a kid");
+  }
+  else {
+    console.log("You are a grown up");
+  }
 }
 
 function ternaryTeenager(age) {
-
-}
+  return ((age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager");
 
 function switchAge(age) {
 
