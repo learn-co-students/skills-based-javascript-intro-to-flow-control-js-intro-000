590b3e96802c71e773f6e8543585e26cb23dcf9a
Danny Radden <dradden@gmail.com>
2017-02-25 23:05:15 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -24,10 +24,21 @@ function ageChecker(age) {
   }
 }
 
-function ternaryTeenager(age) {
-  age >= 13 && age <= 19 ? return "You are a teenager" : return "You are not a teenager";
+function ternaryTeenager(age){
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
-function switchAge(age) {
-
+function switchAge(age){
+  switch (age) {
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+    return "You are a teenager";
+  default:
+    return "You have an age"
+  }
 }
