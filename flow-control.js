01dafe43128a1287cec512f1502521646d3bd2c0
zachary clancy <zachary.j.clancy@gmail.com>
2017-02-26 05:00:49 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,23 +1,43 @@
-function basicTeenager(age) {
-  if(age < 13) 
-  return false
+var basicTeenager = function(age) {
+  if((age >= 13) && (age <=19))
+  return "You are a teenager!";
+}
+
+var teenager = function(age) {
+  if((age >=13) && (age <=19))
+  return "You are a teenager!";}
   else {
-  return("You are a teenager!")
-}
-}
-
-function teenager(age) {
-
+    return "You are not a teenager";
 }
 
-function ageChecker(age) {
+var ageChecker = function(age) {
+  if((age >=13) && (age <=19))
+  return "You are a teenager!";}
+  else if {(age < 13)
+    return "You are a kid" }
+    else { (age > 19)
+    return "You are a grownup";}
 
+
+var ternaryTeenager= function(age) {
+  age => 13 && age <=19?
+  "You are a teenager:You are not a teenager";
 }
 
-function ternaryTeenager(age) {
-
+var switchAge= function(age) {
+var age = ""
+switch (age){
+  case "13":
+  case "14":
+  case "15":
+  case "15":
+  case "16":
+  case "17":
+  case "18":
+  case "19":
+  return "You are a teenager"
+  break;
+  return "You have an age!";
+  default:
 }
-
-function switchAge(age) {
-
 }
