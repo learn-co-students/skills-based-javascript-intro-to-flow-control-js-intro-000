b6ebe796ba984914295a2dc0d058158f5afeba75
Leslie Mollner <lamollner@gmail.com>
2017-02-27 23:56:51 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,16 @@
 function basicTeenager(age) {
-
+  if(age >= 13 && age <= 19) {
+    return "You are a teenager!";
+}
 }
 
 function teenager(age) {
-
+  if(age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
+  else(age < 13 || age > 19) {
+    return "You are not a teenager!";
+  }
 }
 
 function ageChecker(age) {
