7009776150f758c15fe93cfc3cb57c5f34ceaee8
Nayeli C <nayelicruz12@gmail.com>
2017-02-28 03:28:11 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -23,11 +23,21 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-    if(age >= 13 && age <= 19) {
-      return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
+      return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
     }
-}
+    ternaryTeenager(25);
 
 function switchAge(age) {
-
+    switch (age) {
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+        return "You are a teenager"
+      default:
+        return "You have an age"
+    }
 }
