8fee7d0fa0239ae13351164b21e051165e1d4391
Aellon Krider <aellonkrider@gmail.com>
2017-03-03 18:56:54 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,42 @@
-function basicTeenager(age) {
 
+
+var basicTeenager = function basicTeenager(age) {
+  if(age =>13 && <= 19) {
+  return("You are a teenager!")
+  }
 }
 
-function teenager(age) {
-
+var teenager = function teenager(age) {
+  if (age =>13 && <= 19) {
+  return("You are a teenager!")
+  } else {
+  return("You are not a teenager")
+  }
 }
 
-function ageChecker(age) {
-
+var ageChecker = function ageChecker(age) {
+  if(age =>13 && <= 19) {
+  return("You are a teenager!")
+} else if (age <= 12) {
+  return("You are a kid")
+} else {
+  return("You are a grownup")
+}
 }
 
-function ternaryTeenager(age) {
-
+var ternaryTeenager = function ternaryTeenager(age) {
+  if(age =>13 && <= 19) {
+    return("You are a teenager!"):return("You are not a teenager");
+  }
 }
 
-function switchAge(age) {
-
+var switchAge = function switchAge(age) {
+  var age = (13 || 14 || 15 || 16 || 17 || 18 || 19)
+  switch (age) {
+    case "age":
+      console.log("You are a teenager")
+      break;
+    default:
+    console.log("You have an age");
+      }
 }
