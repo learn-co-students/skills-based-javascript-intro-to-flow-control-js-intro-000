0205a8f83b8f5e3ad39ff8f22b608a37575df4e2
Aellon Krider <aellonkrider@gmail.com>
2017-03-06 16:55:45 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,42 +1,55 @@
-
-
-var basicTeenager = function basicTeenager(age) {
-  if(age >= 13 && <= 19) {
-  return("You are a teenager!")
-  }
+function basicTeenager(age) {
+    if (age >= 13 && age <= 19) {
+      return ("You are a teenager!");
+    }
 }
 
-var teenager = function teenager(age) {
-  if (age >= 13 && <= 19) {
-  return("You are a teenager!")
+function teenager(age) { 
+  if (age >= 13 && age <= 19) {
+    return("You are a teenager!");
   } else {
-  return("You are not a teenager")
+    return("You are not a teenager");
   }
 }
 
-var ageChecker = function ageChecker(age) {
-  if(age >= 13 && <= 19) {
-  return("You are a teenager!")
+function ageChecker(age) {
+  if(age >= 13 && age <= 19) {
+    return("You are a teenager!");
 } else if (age <= 12) {
-  return("You are a kid")
+    return("You are a kid");
 } else {
-  return("You are a grownup")
+    return("You are a grownup");
 }
 }
 
-var ternaryTeenager = function ternaryTeenager(age) {
-  if(age >= 13 && <= 19) {
-    return("You are a teenager!"):return("You are not a teenager");
-  }
+function ternaryTeenager(age) {
+  return (age >= 13 && age <= 19) ? ("You are a teenager"): ("You are not a teenager");
 }
 
-var switchAge = function switchAge(age) {
-  var age = (13 || 14 || 15 || 16 || 17 || 18 || 19)
-  switch (age) {
-    case "age":
-      console.log("You are a teenager")
+function switchAge(age) {
+  switch(age) {
+    case 13:
+      return("You are a teenager");
       break;
-    default:
-    console.log("You have an age");
+      case 14:
+      return("You are a teenager");
+      break;
+      case 15:
+      return("You are a teenager");
+      break;
+      case 16:
+      return("You are a teenager");
+      break;
+      case 17:
+      return("You are a teenager");
+      break;
+      case 18:
+      return("You are a teenager");
+      break;
+      case 19:
+      return("You are a teenager");
+      break;
+      default:
+      return("You have an age");
       }
 }
