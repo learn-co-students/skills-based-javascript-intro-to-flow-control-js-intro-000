05bb18d6d612e21a7463a1244d13d63c0f94e015
prabhu kiran bandi <prabhuthe.king143@gmail.com>
2017-03-31 21:33:47 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -26,19 +26,20 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  if (age >= 13 && age <= 19){
-    return "You are a teenager"
-      }
-      else {
-        return "You are not a teenager"
-      }
+  return age >= 13 && age <= 19? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-  if (age >= 13 && age <= 19){
-    return "You are a teenager"
-      }
-      else {
-        return "You have an age"
-      }
+switch(age){
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+  return "You are a teenager"
+  default:
+  return "You have an age"}
+
 }
