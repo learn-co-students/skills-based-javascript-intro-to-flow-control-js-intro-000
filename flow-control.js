543369f98d9fcc63b4a8d9b3b01d9a4dea26b632
Susan Kum <kum.susan@gmail.com>
2017-05-06 19:34:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,4 @@
-function basicTeenager(age) {
-
-}
-
-function teenager(age) {
-
-}
-
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
+function basicTeenager(age){
+  if(age>=13 && age<=19){
+    return "You are a teenager!"}
 }
