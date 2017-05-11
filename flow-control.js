fd1691d1d2992fe85bcd3ef5b1c921d3d8710607
Vickie Wonders <vickiekimondo@gmail.com>
2017-05-11 23:14:58 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,16 +1,28 @@
 function basicTeenager(age) {
-
+  if(age 13 > 19)
+  // "You are a teenager"
 }
 
 function teenager(age) {
-
+  if(age > 19)
+  // "You are a teenager"
+}else {
+  if(age < 19)
+  // "You are not a teenager"
 }
 
 function ageChecker(age) {
-
+  if (age > 12)
+  // "You are a kid"
+} else if (age <= 12) {
+  // "You are a teenager"
+} else {
+  if(age < 19)
+  // "You are a grownup"
 }
 
 function ternaryTeenager(age) {
+  return age 13 - 19 ? "You are a teenager" : "You are not a teenager"
 
 }
 
