00ececed418451916c17a1dbf79079c44b76c798
Jagat Tuladhar <jagatjyoti.1si13cs040@gmail.com>
2017-05-20 17:40:56 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -68,11 +68,11 @@ describe('flow-control', () => {
   })
 
   describe('switchAge', () => {
-    it('should return "You are a teenager" if age is between 13-19', () => {
+    it('should return "You are a teenager" if age is between 13-191', () => {
       expect(switchAge(15)).toEqual("You are a teenager")
     })
 
-    it('should return "You have an age" if age not between 13-19', () => {
+    it('should return "You have an age" if age not between 13-192', () => {
       expect(switchAge(75)).toEqual("You have an age")
       expect(switchAge(7)).toEqual("You have an age")
     })
