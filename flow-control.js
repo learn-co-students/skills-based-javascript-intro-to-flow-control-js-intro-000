a1da49c3a4416bbc05bf4c95ac8fc3d9016889cb
Ansel Bobrow <ansel.bobrow@gmail.com>
2017-05-23 01:44:42 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,52 @@
 function basicTeenager(age) {
 
+  if (13 <= age && age <= 19) {
+    return "You are a teenager!"
+  }
+
 }
 
 function teenager(age) {
 
+  if (13 <= age && age <= 19) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
+
 }
 
 function ageChecker(age) {
-
+  if (13 <= age && age <= 19) {
+    return "You are a teenager!"
+  } else if (age < 13) {
+    return "You are a kid"
+  } else {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return 13 <= age && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+      return "You are a teenager"
+    case 14:
+      return "You are a teenager"
+    case 15:
+      return "You are a teenager"
+    case 16:
+        return "You are a teenager"
+    case 17:
+        return "You are a teenager"
+    case 18:
+      return "You are a teenager"
+    case 19:
+      return "You are a teenager"
+    default:
+      return "You have an age"
+  }
 }
