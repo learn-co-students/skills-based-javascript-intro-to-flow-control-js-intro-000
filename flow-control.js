aead1c2eaf0f4a7f65c617f03b03b64ff491c133
Micah  Onditi <micahonditi@gmail.com>
2017-06-02 00:41:34 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,64 @@
+
 function basicTeenager(age) {
 
+  if (age >=13 && age <=19) {
+
+    return "You are a teenager!"
+  }
 }
+basicTeenager(14)
 
 function teenager(age) {
 
+  if (age >=13 && age <=19) {
+
+    return "You are a teenager!"
+  }
+
+  else {
+
+    return "You are not a teenager"
+  }
+
 }
+teenager(12)
+
 
 function ageChecker(age) {
 
+  if (age >=13 && age <=19) {
+
+    return "You are a teenager!"
+  }
+  else if ( age <= 12) {
+    return "You are a kid"
+  }
+  else if ( age >19) {
+    return "You are a grownup"
+  }
+
 }
+ageChecker(21)
+
 
 function ternaryTeenager(age) {
-
+  return check = (age >=13 && age <=19)? "You are a teenager" : "You are not a teenager"
 }
+ternaryTeenager(15)
 
 function switchAge(age) {
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager"
+    default:
+      return "You have an age"
+  }
 
 }
+switchAge(21)
