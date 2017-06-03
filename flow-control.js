69afc4ea1cd7b999fde1b0e63d134b0a4cca1f58
Monica Serafini <vegaswebgirl@gmail.com>
2017-06-03 02:12:04 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,46 @@
 function basicTeenager(age) {
-
+ if (age >= 13 && age <= 19) {
+  return "You are a teenager!";
+ }
 }
 
 function teenager(age) {
-
+if (age >= 13 && age <= 19) {
+  return "You are a teenager!";
+  } else {
+  return "You are not a teenager";
+ }
 }
 
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
-}
+// function ternaryTeenager(age) {
+// return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager";
+// }
+//
+// function switchAge(age) {
+//   switch(age) {
+//   var Age = "13"
+//     case "13":
+//       console.log("You are a teenager");
+//       break;
+//     case "14":
+//       console.log("You are a teenager");
+//       break;
+//     case "15":
+//     console.log("You are a teenager");
+//       break;
+//       case "16":
+//       console.log("You are a teenager");
+//       break;
+//     case "17":
+//       console.log("You are a teenager");
+//       break;
+//     case "18":
+//       console.log("You are a teenager");
+//       break;
+//     case "19":
+//       console.log("You are a teenager");
+//       break;
+//     default:
+//      return ("You have an age");
+//   }
+// }
