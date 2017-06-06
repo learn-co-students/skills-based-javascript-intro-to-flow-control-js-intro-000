1bca4adc1f1671d33ee2257df0932a6d1c36a140
Jennifer Volpe <jvolpe721@gmail.com>
2017-06-06 15:34:58 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,42 @@
-function basicTeenager(age) {
 
+function basicTeenager (age) {
+    if (age >= 13 && age <= 19) {
+        return "You are a teenager!";
+}
 }
 
-function teenager(age) {
 
-}
+  function teenager(age) {
+      if (age >= 13 && age <= 19) {
+          return "You are a teenager!";
+      } else {
+          return "You are not a teenager";
+      }
+  }
+
 
 function ageChecker(age) {
-
+    if (age >= 13 && age <= 19) {
+        return "You are a teenager!";
+    } else if (age <= 12) {
+        return "You are a kid";
+    } else if (age >= 20){
+      return "You are a grownup";
+    }
 }
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager (age){
+  if (age >= 13 && age <= 19){
+    return "You are a teenager";
+  } else {
+    return "You are not a teenager";
+  }
 }
 
-function switchAge(age) {
-
+function switchAge (age) {
+  if(age >= 13 && age <= 19){
+    return "You are a teenager";
+  } else {
+  return "You have an age";
+  }
 }
