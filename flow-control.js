1ee3f076fd6faacee23831eb9dcbba7d1c6f8e42
Monica Serafini <vegaswebgirl@gmail.com>
2017-06-25 21:26:40 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -11,36 +11,45 @@ if (age >= 13 && age <= 19) {
   return "You are not a teenager";
  }
 }
+function ageChecker(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else if (age <= 12) {
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
+}
 
-// function ternaryTeenager(age) {
-// return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager";
-// }
-//
-// function switchAge(age) {
-//   switch(age) {
-//   var Age = "13"
-//     case "13":
-//       console.log("You are a teenager");
-//       break;
-//     case "14":
-//       console.log("You are a teenager");
-//       break;
-//     case "15":
-//     console.log("You are a teenager");
-//       break;
-//       case "16":
-//       console.log("You are a teenager");
-//       break;
-//     case "17":
-//       console.log("You are a teenager");
-//       break;
-//     case "18":
-//       console.log("You are a teenager");
-//       break;
-//     case "19":
-//       console.log("You are a teenager");
-//       break;
-//     default:
-//      return ("You have an age");
-//   }
-// }
+function ternaryTeenager(age) {
+return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager";
+}
+
+function switchAge(age) {
+  switch(age) {
+    case 13:
+      return("You are a teenager");
+      break;
+    case 14:
+      return("You are a teenager");
+      break;
+    case 15:
+      return("You are a teenager");
+      break;
+    case 16:
+      return("You are a teenager");
+      break;
+    case 17:
+      return("You are a teenager");
+      break;
+    case 18:
+      return("You are a teenager");
+      break;
+    case 19:
+       return("You are a teenager");
+       break;
+
+     default:
+        return("You have an age");
+  }
+}
