5141966df2bd1960998ba465b2448c492ff4516a
D Fayth <danae.fm19@yahoo.com>
2017-07-13 06:42:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -16,7 +16,7 @@ function ageChecker(age) {
     return "You are a teenager!";
   } else if (age <= 12) {
     return "You are a kid";
-  } else if (age > 19) {
+  } else if (age > 18) {
     return "You are a grownup";
   }
 }
