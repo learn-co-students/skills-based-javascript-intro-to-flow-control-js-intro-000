ae10b6f1a40639c8121293a8e9a7c8d74eaccbae
Alex Poulos <alexpoulos31@gmail.com>
2017-07-13 03:07:27 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,20 @@
 function basicTeenager(age) {
-
+   if (age >= 13 && <= 19) {
+    return "You are a teenager!";
+  }
 }
-
-function teenager(age) {
-
-}
-
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
-}
+// function teenager(age) {
+//
+// }
+//
+// function ageChecker(age) {
+//
+// }
+//
+// function ternaryTeenager(age) {
+//
+// }
+//
+// function switchAge(age) {
+//
+// }
