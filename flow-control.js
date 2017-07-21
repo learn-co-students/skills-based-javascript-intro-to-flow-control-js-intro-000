94e3a033fd533d0af13388bba74d7f63db19e72f
Iñigo Solano <1334@shadowproject.net>
2017-07-21 18:37:35 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,36 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
+  if (!basicTeenager(age)) {
+    return "You are not a teenager";
+  } else {
+    return "You are a teenager!";
+  }
 }
 
 function ageChecker(age) {
-
+  if (age <= 12) {
+    return "You are a kid"
+  } else if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else if (age >= 20) {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
-
+  switch (true) {
+    case (age >= 13 && age <= 19):
+    return "You are a teenager";
+    default:
+    return "You have an age";
+  }
 }
