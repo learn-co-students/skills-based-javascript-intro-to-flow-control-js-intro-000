f3e9c56f0590dedff1ea5e3fe60fa0b0f03beb8c
Tina Murphy <tina.kep@me.com>
2017-07-24 02:07:42 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -19,7 +19,8 @@ function ageChecker(age)
   ("You are a grownup")
 }
 function ternaryTeenager(age) {
-
+  var age = >= 13 && <= 19;
+  console.log("You are a teenager" : "You are not a teenager")
 }
 
 function switchAge(age) {
