fdcdb301c94e2f4b49d70ec01bb757ebae09ce0c
Steven Oladipo <stevenboladipo@gmail.com>
2017-08-02 02:10:14 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,12 @@
-function basicTeenager(age) {
 
-}
+function basicTeenager(age) {
+      var age = 15;
+  if (age >= 13 || age <= 19) {
+     return "You are a teenager!";
+   } else
+        return "You are not a teenager.";
+ }
+
 
 function teenager(age) {
 
