1651c8a51653b2b9d7d0ca5c07a2f082ae336b8c
Steven Oladipo <stevenboladipo@gmail.com>
2017-08-02 22:16:34 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -9,7 +9,10 @@ function basicTeenager(age) {
 
 
 function teenager(age) {
-
+        var age = 10
+      if (age >= 13 || age <= 19) { return "You are a teenager!";}
+      else if (age <= 12) {return "You are a kid.";}
+      else {return "You are a grownup.";}
 }
 
 function ageChecker(age) {
