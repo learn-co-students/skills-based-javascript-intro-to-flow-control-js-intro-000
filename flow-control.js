604cc39cd5ee960dbd00d75549405be91966e9ac
Steven Oladipo <stevenboladipo@gmail.com>
2017-08-03 00:55:10 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -20,8 +20,8 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-var age = (age >= 13 || age <= 19) ? "You are a teenager.":"You are not a teenager.";
-      return age = 15 ? "You are a teenager.":"You are not a teenager."
+   (age >= 13 || age <= 19) ? "You are a teenager.":"You are not a teenager.";
+      return age = 9 ? "You are a teenager":"You are not a teenager."
 }
 
 function switchAge(age) {
