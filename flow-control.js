134717452de4f26708fe46274d900de71faef229
Steven Oladipo <stevenboladipo@gmail.com>
2017-08-03 01:06:54 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -25,5 +25,31 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
+  case "13":
+  console.log("You are a teenager");
+  break;
 
+  case "14":
+  console.log("You are a teenager");
+  break;
+
+  case "15":
+  console.log("You are a teenager");
+  break;
+
+  case "16":
+  console.log("You are a teenager");
+  break;
+
+  case "17":
+  console.log("You are a teenager");
+  break;
+
+  case "18":
+  console.log("You are a teenager");
+  break;
+
+  case "19":
+  console.log("You are a teenager");
+  break;
 }
