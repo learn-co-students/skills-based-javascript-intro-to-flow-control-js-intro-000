ba65ef6d7785362bdc51e176316eb94dbc661d6a
Steven Oladipo <stevenboladipo@gmail.com>
2017-08-04 22:19:44 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,50 +1,55 @@
 
 function basicTeenager(age) {
       var age = 15;
-  if (age >= 13 || age <= 19) {
+  if (age >= 13 && age <= 19) {
      return "You are a teenager!";
-   } else
-        return "You are not a teenager.";
+   } else{
+        return "You are not a teenager";}
  }
 
 
 function teenager(age) {
-        var age = 11
+        var age = 11;
       if (age >= 13 || age <= 19) { return "You are a teenager!";}
       else if (age <= 12) {return "You are a kid.";}
-      else {return "You are a grownup.";}
+      //else (age >= 20) {return "You are a grownup.";}
 }
 
 function ageChecker(age) {
-
+    var age = 10;
+  if ( age >= 13 && age <= 19) {return "You are a teenager!"}
+else if (age <= 12) {return "You are a kid"}
+else (age >= 20) {return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
-   (age >= 13 || age <= 19) ? "You are a teenager.":"You are not a teenager.";
-      return age = 9 ? "You are a teenager":"You are not a teenager."
+   return (age >= 13 && age <= 19) ? "You are a teenager":"You are not a teenager";
 }
 
 function switchAge(age) {
-  age = 13
-  console.log("You are a teenager");
+switch(age) {
+  case 13 :
+  return "You are a teenager";
   break;
-  age = 14
-  console.log("You are a teenager");
+  case 14 :
+  return "You are a teenager";
   break;
-  age = 15
-  console.log("You are a teenager");
+  case 15 :
+  return "You are a teenager";
   break;
-  age = 16
-  console.log("You are a teenager");
+  case 16 :
+  return "You are a teenager";
   break;
-  age = 17
-  console.log("You are a teenager");
+  case 17 :
+  return "You are a teenager";
   break;
-  age = 18
-  console.log("You are a teenager");
+  case 18 :
+  return "You are a teenager";
   break;
-  age = 19
-  console.log("You are a teenager");
+  case 19 :
+  return "You are a teenager";
   break;
+  default:
      return "You have an age";
 }
+  }
