04828e18464dd560a31454f2202f0af15c23f52f
Tina Murphy <tina.kep@me.com>
2017-08-25 05:00:11 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -22,19 +22,25 @@ function switchAge(age) {
   switch(age) {
     case 13:
       return "You are a teenager";
+      break;
     case 14:
        return "You are a teenager";
+       break;
     case 15:
        return "You are a teenager";
+       break;
     case 16:
        return "You are a teenager";
+       break;
     case 17:
        return "You are a teenager";
+       break;
     case 18:
        return "You are a teenager";
+       break;
     case 19:
        return "You are a teenager";
-
+       break;
     default:
     console.log ("You have an age");
     }
