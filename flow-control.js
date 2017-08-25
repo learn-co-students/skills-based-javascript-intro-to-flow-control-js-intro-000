6655018857617885ec7724c70ab7b9a6a26ba62e
Tina Murphy <tina.kep@me.com>
2017-08-25 05:26:25 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -9,13 +9,16 @@ function teenager(age) {
     return "You are a teenager!"
   }
 }
-   function ageChecker(age) {
-          if (age>= 13 && age<= 19) {
-       return"You are a kid";
-   } else {
-       return"You are a grownup";
-     }
-       }
+
+  function ageChecker(age) {
+    if (age >= 13 && age <=19) {
+      return "You are a teenager!"
+    } else if (age<=12) {
+      return "You are a kid"
+    } else {
+      return "You are a grownup"
+    }
+  }
 
 
        function ternaryTeenager(age)  {
