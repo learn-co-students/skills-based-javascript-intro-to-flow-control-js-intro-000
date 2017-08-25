7e932462f745bc8c4dd7e5cf7d3ad5f1d991741f
Tina Murphy <tina.kep@me.com>
2017-08-25 05:16:25 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,14 @@
 function basicTeenager(age) {
         if (age >= 13 && age <= 19) {
-        return"You are a teenager!";
+        return"You are a teenager!"
       }
    }
 
+function teenager(age) {
+  if (age >=13 && <=19) {
+    return "You are a teenager!"
+  }
+}
    function ageChecker(age) {
           if (age>= 13 && age<= 19) {
        return"You are a kid";
