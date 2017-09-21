6146a8ada8700ce88b64b8dc6dbb29f826039d75
Othman AMD <red.fantastic@hotmail.com>
2017-09-21 22:41:26 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,55 @@
 function basicTeenager(age) {
-
+if (age > 12 && age < 20){
+  return "You are a teenager!";
+}
 }
 
 function teenager(age) {
-
+  if (age > 12 && age < 20){
+    return "You are a teenager!";
+  }
+  else{
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
-
+  if (age > 12 && age < 20){
+    return "You are a teenager!";
+  }
+  else if (age <= 12){
+    return "You are a kid";
+  } else{
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
-
+  if (age > 12 && age < 20){
+    return "You are a teenager";
+  } else{
+    return "You are not a teenager";
+  }
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+      break;
+    case 14:
+      break;
+    case 15:
+      break;
+    case 16:
+      break;
+    case 17:
+      break;
+    case 18:
+      break;
+    case 19:
+      break;
+    default:
+      return "You have an age";
+  }
+  return "You are a teenager";
 }
