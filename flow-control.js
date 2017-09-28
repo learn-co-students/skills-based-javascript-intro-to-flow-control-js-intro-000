01c12a6c96ebeecdf7b0af8bc6299e1cbb85af96
Yoni Slotwiner <yonislots@gmail.com>
2017-09-28 20:59:49 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,61 @@
 function basicTeenager(age) {
-
-}
+  if(age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
+};
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else {
+    return "You are not a teenager";
+  }
 }
+
+teenager(14);
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+} else if (age <= 12) {
+  return "You are a kid";
+} else if (age > 19) {
+  return "You are a grownup";
 }
+ageChecker(15);
 
 function ternaryTeenager(age) {
-
+  return (age >= 13 && age <= 19) ? "You are a teenager!" : "You are not a teenager";
 }
+
+ternaryTeenager(16);
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+    console.log("You are a teenager");
+    break;
+    case 14:
+    console.log("You are a teenager");
+    break;
+    case 15:
+    console.log("You are a teenager");
+    break;
+    case 16:
+    console.log("You are a teenager");
+    break;
+    case 17:
+    console.log("You are a teenager");
+    break;
+    case 18:
+    console.log("You are a teenager");
+    break;
+    case 19:
+    console.log("You are a teenager");
+    break;
+    default:
+    console.log("You have an age");
+  }
 }
+
+switchAge(17)
