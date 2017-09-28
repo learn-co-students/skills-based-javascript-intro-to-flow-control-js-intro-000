d2f85bf50cf54c13f9e44363a197d1c6a3f83aa8
Yoni Slotwiner <yonislots@gmail.com>
2017-09-28 21:10:21 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -4,6 +4,8 @@ function basicTeenager(age) {
   }
 };
 
+basicTeenager(13);
+
 function teenager(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
@@ -17,15 +19,17 @@ teenager(14);
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
-} else if (age <= 12) {
+  } else if (age <= 12) {
   return "You are a kid";
 } else if (age > 19) {
   return "You are a grownup";
-}
+  }
+};
+
 ageChecker(15);
 
 function ternaryTeenager(age) {
-  return (age >= 13 && age <= 19) ? "You are a teenager!" : "You are not a teenager";
+  return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager";
 }
 
 ternaryTeenager(16);
@@ -33,28 +37,28 @@ ternaryTeenager(16);
 function switchAge(age) {
   switch (age) {
     case 13:
-    console.log("You are a teenager");
+    return "You are a teenager";
     break;
     case 14:
-    console.log("You are a teenager");
+    return "You are a teenager";
     break;
     case 15:
-    console.log("You are a teenager");
+    return "You are a teenager";
     break;
     case 16:
-    console.log("You are a teenager");
+    return "You are a teenager";
     break;
     case 17:
-    console.log("You are a teenager");
+    return "You are a teenager";
     break;
     case 18:
-    console.log("You are a teenager");
+    return "You are a teenager";
     break;
     case 19:
-    console.log("You are a teenager");
+    return "You are a teenager";
     break;
     default:
-    console.log("You have an age");
+    return "You have an age";
   }
 }
 
