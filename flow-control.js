dd338d5581fd9cf8e50147dfeb53a2cc3b15569e
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-10-03 19:22:09 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,47 +1,47 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19)
-  return("You are a teenager!")
+  return "You are a teenager!"
 }
 
 function teenager(age) {
   if (age >= 13 && age <= 19)
-  return("You are a teenager!")
-  else
-  return("You are not a teenager")
+  return "You are a teenager!"
+  // else
+  return "You are not a teenager"
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19)
-  return("You are a teenager!")
+  return "You are a teenager!"
   else if (age < 13)
-  return("You are a kid")
+  return "You are a kid"
   else (age > 19)
-  return("You are a grownup")
+  return "You are a grownup"
 }
 
 function ternaryTeenager(age) {
   return (age >= 13 && age <= 19) ?
-    ("You are a teenager") :
-    ("You are not a teenager")
+    "You are a teenager" :
+    "You are not a teenager"
 }
 
 function switchAge(age) {
  switch(age) {
-  case "13":
+  case 13:
    return "You are a teenager"
-  case "14":
+  case 14:
    return "You are a teenager"
-  case "15":
+  case 15:
    return "You are a teenager"
-  case "16":
+  case 16:
    return "You are a teenager"
-  case "17":
+  case 17:
    return "You are a teenager"
-  case "18":
+  case 18:
    return "You are a teenager"
-  case "19":
+  case 19:
    return "You are a teenager"
   default:
    return "You have an age"
- }}
- 
+ }
+}
