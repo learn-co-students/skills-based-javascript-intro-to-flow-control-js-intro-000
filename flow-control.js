d777d9bf43a225ede91733e6774fed4a596e2131
Eric Sherman <esherman9@gmail.com>
2017-10-10 03:57:50 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,15 @@
 function basicTeenager(age) {
-
+if age > 13 && age < 20 {
+  return "You are a teenager"
+}
 }
 
 function teenager(age) {
-
+  if age > 13 && age < 20 {
+    return "You are a teenager"
+} else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
