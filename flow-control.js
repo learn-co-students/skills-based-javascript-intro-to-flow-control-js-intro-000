3f00d783c8ed03a8a21e5686d6d4ffd154d4de79
Eric Sherman <esherman9@gmail.com>
2017-10-10 04:09:16 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -28,5 +28,14 @@ return age < 20 && age > 12 ? "You are a teenager!" : "You are not a teenager"
 }
 
 function switchAge(age) {
+switch(age) {
+  case age < 20 && age >=13:
+  return "You are a teenager!"
+  break;
 
+  default:
+  return "You have an age"
+  break;
+  
+}
 }
