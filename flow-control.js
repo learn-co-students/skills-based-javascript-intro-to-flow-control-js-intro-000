e3e9cb28aeec729fee477a31415b6fcf92c70b36
Joshua Palacios <palacios.joshua@gmail.com>
2017-10-11 23:02:09 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -22,19 +22,21 @@ function ageChecker(age) {
   }
 }
 
-function ternaryTeenager(age) {
-  if (age >= 13 && age <= 19) {
-    return "You are a teenager"
-  } else {
-    return "You are not a teenager"
-  }
-
+function ternaryTeenager(age){
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
-function switchAge(age) {
-  if (age >= 13 && age <= 19) {
-    return "You are a teenager"
-  } else {
+function switchAge(age){
+  switch (age) {
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+    return "You are a teenager";
+  default:
     return "You have an age"
   }
 }
