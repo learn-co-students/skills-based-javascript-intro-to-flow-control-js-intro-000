6c60d22d663bbf016df7341b7348491ce095a04a
Hamsa Alchllk <hamsa.b.ch@gmail.com>
2017-10-24 19:16:15 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
 if Teenager(13-19) return{(`You Are A Teenager!`)
 }
 else (age===>19) return{(`You Are An Adult!`);
-  else (age===<13)return{(`You Are A Kid!`)
+  else if(age===<13)return{(`You Are A Kid!`)
   }
