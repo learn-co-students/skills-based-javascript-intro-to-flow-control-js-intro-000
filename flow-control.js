9713ee2242f3169d35fc4d43088ad65e8302a585
Hamsa Alchllk <hamsa.b.ch@gmail.com>
2017-10-24 21:38:05 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -5,5 +5,5 @@ function AgeCheckerif(age===12 || age<12) Go {('You Are A Kid!')
 }else if(age>19) Go {('You Are An Adult!')
 }
 function (ternaryTeenager 13-19){('13-19')}
-ValueToBeReturned (True) :{('You Are A Teenager!')}
-ValueToBeReturned (False):{('You Are Not A Teenager!')}
+ValueToBeReturned (True){('You Are A Teenager!')}
+ValueToBeReturned (False){('You Are Not A Teenager!')}
