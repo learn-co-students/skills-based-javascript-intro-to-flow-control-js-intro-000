3c9e7201fe77b33e5b1384617f84285324daee25
Hamsa Alchllk <hamsa.b.ch@gmail.com>
2017-10-24 18:37:44 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,6 @@
-var = Age;
-function basicTeenager1 ifAgebtween("13-19"){
-  console.log(`you are a teenager!, ${basicTeenager}!`)
+var (Age)
+function basicAge("13-19") {return
+  console.log(`you are a teenager! , ${basicTeenager}!`)
 }
 else teenager2Age{console.log("Age=<13 && 19=<Age");return("you are not a teenager!");
 }
@@ -13,11 +13,3 @@ else if(Age<13)say{"you are a kid!";} if(Age>19) say{"you are a grownup!";
  (12>=,20=<) : {"You are not a teenager!";
 }
 function switchAge(teenager)
-case "teenager":
-console.log("13-19");
-break;
-case "olderly":
-console.log("you are too old!");
-break; default:
-console.log("you have an age!");
-}
