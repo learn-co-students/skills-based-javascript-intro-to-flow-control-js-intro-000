286fbca5583ece7923108ee49cc189db340f7ea2
Hamsa Alchllk <hamsa.b.ch@gmail.com>
2017-10-24 21:06:03 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,6 @@
-function BasicTeenagerBtween(13-19) return{'You Are A Teenager!'
+function(BasicTeenagerBtween13-19) return {'You Are A Teenager!'
 }else {'You Are Not A Teenager!'
 }
-function AgeChecker
-if(age===12 || age>12) Go {('You Are A Kid!')
+function AgeCheckerif(age===12 || age>12) Go {('You Are A Kid!')
 }else if(age>19) Go {('You Are An Adult!')
 }
