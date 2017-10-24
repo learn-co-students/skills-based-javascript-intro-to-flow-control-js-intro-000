aa189f052eb71b708acb5da59d7a29ef7d48eb91
Hamsa Alchllk <hamsa.b.ch@gmail.com>
2017-10-24 21:36:49 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,9 @@
-function(BasicTeenagerBtween(13-19))return {'You Are A Teenager!'
-}else {'You Are Not A Teenager!'
+function(BasicTeenagerBtween(13-19))return {('You Are A Teenager!')
+}else {('You Are Not A Teenager!')
 }
-function AgeCheckerif(age===12 || age<12) Go {'You Are A Kid!'
-}else if(age>19) Go {'You Are An Adult!'
+function AgeCheckerif(age===12 || age<12) Go {('You Are A Kid!')
+}else if(age>19) Go {('You Are An Adult!')
 }
-function (ternaryTeenager 13-19){'13-19'}
-ValueToBeReturned (True) :{'You Are A Teenager!'}
-ValueToBeReturned (False):{'You Are Not A Teenager!'}
+function (ternaryTeenager 13-19){('13-19')}
+ValueToBeReturned (True) :{('You Are A Teenager!')}
+ValueToBeReturned (False):{('You Are Not A Teenager!')}
