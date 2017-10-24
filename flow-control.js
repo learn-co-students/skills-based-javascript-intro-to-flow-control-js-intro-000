04e38bc495bd806c5f2432b6d2dd9aa7951d53e2
Hamsa Alchllk <hamsa.b.ch@gmail.com>
2017-10-24 04:21:59 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,27 +1,23 @@
-function if (basicTeenager=13-14-15-16-17-18-19){return = "You are a teenager!";
+var = Age;
+function basicTeenager1 ifAgebtween("13-19"){
+  console.log(`you are a teenager!, ${basicTeenager}!`)
 }
-else if(Age<13-Age>19) {say"you are not a teenager!";
+else teenager2Age{console.log("Age=<13 && 19=<Age");return("you are not a teenager!");
 }
-ageCheckerifAgeIs(13-19) return{"you are a teenager!";
+else teenager3AgeChecker{
+if"Age"btween(13-19)return
+console.log("you are a teenager!");
 }
-else if (Age<13) {say"you are a kid!";} if(Age>19) {say"you are a grownup!";
+else if(Age<13)say{"you are a kid!";} if(Age>19) say{"you are a grownup!";
 }
-function ternaryTeenagerevaluationAge(13-19) {return
-}:
-valueToBeReturnedifTrueAge(13-19) {"you are a teenager!";
+ (12>=,20=<) : {"You are not a teenager!";
 }
-valueToBeReturnedifFalseAge(12>,20<) {"You are not a teenager!";
-}
-function switchAge(teenager) {
-case "child":
-console.log("You should be playing in the backyard!");
-break;
+function switchAge(teenager)
 case "teenager":
 console.log("13-19");
 break;
 case "olderly":
-console.log("you are too old for this test!");
-break;
-default:
+console.log("you are too old!");
+break; default:
 console.log("you have an age!");
 }
