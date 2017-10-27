5dd1f2178dee0dc3daab450693abf08fb53702e6
Dylan Jasper <dylaneverettj@gmail.com>
2017-10-27 01:17:56 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,15 @@
+
+
 function basicTeenager(age) {
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!";
+  }
 
 }
 
 function teenager(age) {
-
-}
-
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!";
+  }else {return "You are not a teenager";}
 
 }
