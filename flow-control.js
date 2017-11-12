04105219497705f2b9be1b9e78215655f99efecd
Hamsa Alchllk <hamsa.b.ch@gmail.com>
2017-11-12 09:49:58 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,4 @@
-var basicTeenager =13-19;
-if(basicTeenager 13-19){
-  console.log('you\'re a Teenager!');
+function BasicTeenager() {
+  console.log('You are a teenager!');
 }
-else {
-  console.log('you are not a Teenager');
-}
+BasicTeenager();
