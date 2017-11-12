92e32167dd40823db057b682e629f10229b6ae16
Hamsa Alchllk <hamsa.b.ch@gmail.com>
2017-11-12 10:36:05 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
-function basicTeenagerif(13-19, age<=12, age=>19){
+function basicTeenagerif=(13-19, age<=12, age=>19){
   console.log('You\'re a teenager!'
   + 13-19 + age<=12 + age=>19);
 }
-basicTeenagerif('btween 13-19', "you\'re a kid!","you\'re a grownup!");
+basicTeenagerif=('btween 13-19', "you\'re a kid!","you\'re a grownup!");
