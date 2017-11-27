576414d34b1a292ab8ad8edde57a7a6a2a0d5908
David Clelland <davidclelland6@gmail.com>
2017-11-27 19:41:53 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -27,10 +27,30 @@ return "You are a kid";
   return "You are a grownup";
 }
 }
+
 // ternaryTeenager
 function ternaryTeenager(age) {
  return age >=13 && age <=19 ? "You are a teenager" : "You are not a teenager"}
 
+// switchAge
 function switchAge(age) {
-
+switch(age) {
+  case 13:
+    "You are a teenager";
+    break;
+  case 14:
+   return"You are a teenager";
+  case 15:
+    return"You are a teenager";
+  case 16:
+    return"You are a teenager";
+  case 17:
+    return"You are a teenager";
+  case 18:
+    return"You are a teenager";
+  case 19:
+    return"You are a teenager";
+  default:
+    return"You have an age";
+}
 }
