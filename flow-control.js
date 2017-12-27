289fba89311ce4a64c3a28aef10e1564b680c147
sanober riaz <sanober@sanober.me>
2017-12-27 06:36:35 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -34,9 +34,19 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  if (age >= 13 && age <= 19) {
-    return "You are a teenager";
-  }else {
-    return "You have an age";
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager";
+      break;
+      default:
+      return "You have an age"
+
   }
+
 }
