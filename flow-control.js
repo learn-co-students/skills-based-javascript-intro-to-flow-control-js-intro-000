383ec20332675cece053aab512a5c68a8015112b
ann miller <harleyjane@hotmail.com>
2018-01-11 22:45:35 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,41 @@
 function basicTeenager(age) {
-
+    if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+      }
+  }
+//complete
+function teenager(age){
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager!";
+} else {
+  return "You are not a teenager";}
 }
 
-function teenager(age) {
+function ageChecker(age){
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager!";
+} else if (age <= 12) {
+  return "You are a kid";
+} else {return "You are a grownup"}}
 
+
+
+
+
+//complete
+function ternaryTeenager(age){
+    if (age >= 13 && age <= 19) {
+      return "You are a teenager";
+    } else { (age > 19 && age < 13)
+    return "You are not a teenager";}
 }
 
-function ageChecker(age) {
 
-}
 
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
+//complete
+function switchAge(age){
+if (age >= 13 && age <= 19) {
+  return "You are a teenager";
+} else { (age > 19 && age < 13)
+return "You have an age";}
 }
