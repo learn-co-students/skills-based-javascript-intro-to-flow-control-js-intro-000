c2bc8640438ef1fa67cca10324866ee0680c3bbd
Virginia Herron <inadream@gmail.com>
2018-01-14 02:50:07 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -27,5 +27,29 @@ return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
-
+switch(age) {
+  case "13":
+    console.log("You are a teenager");
+    break;
+  case "14":
+    console.log("You are a teenager");
+    break;
+  case "15":
+    console.log("You are a teenager");
+    break;
+  case "16":
+    console.log("You are a teenager");
+    break;
+  case "17":
+    console.log("You are a teenager");
+    break;
+  case "18":
+    console.log("You are a teenager");
+    break;
+  case "19":
+    console.log("You are a teenager");
+    break;
+  default:
+    console.log("You have an age");
+}
 }
