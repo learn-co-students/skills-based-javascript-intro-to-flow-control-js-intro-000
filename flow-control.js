605d71f9870011da922c8df1240a4b84b6067a20
Virginia Herron <inadream@gmail.com>
2018-01-14 02:34:32 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -13,11 +13,17 @@ if(age >= 13 && age <= 19) {
 }
 
 function ageChecker(age) {
-
+if(age >= 13 && age <= 19) {
+  return "You are a teenager!";
+} else if(age < 13) {
+  return "You are a kid";
+} else {
+  return "You are a grownup";
+}
 }
 
 function ternaryTeenager(age) {
-
+return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
