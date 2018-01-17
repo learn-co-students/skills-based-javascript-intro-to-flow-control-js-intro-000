240906fc3deda23b0a9fc6e19b3eeba6b8149c05
Nataly <nataly247@gmail.com>
2018-01-17 18:23:56 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+  return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
