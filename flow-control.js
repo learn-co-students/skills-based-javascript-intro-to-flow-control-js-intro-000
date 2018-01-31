0cb527e8cdfa5d283a4fcade6e98c799e21e0f4f
Amanda Segal <amandarose31@gmail.com>
2018-01-31 02:47:57 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,61 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+  return `You are a teenager!`
+ }
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+  return `You are a teenager!`
+ }
+ else {
+   return `You are not a teenager`
+ }
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+  return `You are a teenager!`
+ }
+ else if (age <= 12){
+   return `You are a kid`
+ }
+ else if (age >= 20){
+   return 'You are a grownup'
+ }
+ else {
+   return 'Please enter a valid age'
+ }
 }
 
 function ternaryTeenager(age) {
-
+  return (age <= 19 && age >= 13 ? "You are a teenager" : "You are not a teenager");
 }
 
 function switchAge(age) {
-
+switch(age) {
+  case 13:
+    console.log("You are a teenager");
+    break;
+  case 14:
+    console.log("You are a teenager");
+    break;
+  case 15:
+    console.log("You are a teenager");
+    break;
+  case 16:
+    console.log("You are a teenager");
+    break;
+  case 17:
+    console.log("You are a teenager");
+    break;
+  case 18:
+    console.log("You are a teenager");
+    break;
+  case 19:
+    console.log("You are a teenager");
+    break;
+  default:
+    console.log("You have an age");
+}
 }
