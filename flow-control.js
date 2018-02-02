bc5c94814c5fd3e80c573717ada01e6ff5309c42
bkhartmire <bkhartmire@gmail.com>
2018-02-02 21:06:52 +0100

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,25 @@
 function basicTeenager(age) {
-
+  if (age < 20 && age > 12) {
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if (age < 20 && age > 12) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if (age < 20 && age > 12) {
+    return "You are a teenager!"
+  } else if age < 13 {
+    return "You are a kid"
+  } else if age > 19 {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
