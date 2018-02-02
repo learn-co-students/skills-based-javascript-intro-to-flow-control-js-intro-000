c94d8d992c0fc6a2dae881687390da185f29dc02
bkhartmire <bkhartmire@gmail.com>
2018-02-02 21:49:56 +0100

    Done.

--- flow-control.js
+++ flow-control.js
@@ -15,17 +15,28 @@ function teenager(age) {
 function ageChecker(age) {
   if (age < 20 && age > 12) {
     return "You are a teenager!"
-  } else if age < 13 {
+  } else if (age < 13) {
     return "You are a kid"
-  } else if age > 19 {
+  } else if (age > 19) {
     return "You are a grownup"
   }
 }
 
 function ternaryTeenager(age) {
-
+  return (age > 12 && age < 20) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch(age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager"
+    default:
+      return "You have an age"
+  }
 }
