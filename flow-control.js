cbc7d676800e4e00dfff27d7d1d1280700baf56a
Alex Segers <a.segers.dev@gmail.com>
2018-02-11 03:30:17 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -21,5 +21,16 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  return age > 12 && age < 20 ? "You are a teenager" : "You have an age"
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager";
+    default:
+      return "You have an age"
+  }
 }
