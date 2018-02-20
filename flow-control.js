1816d5317bfc6ee34850f3b12c57506cdc2cd59b
henrysteckel@gmail.com <henrysteckel@gmail.com>
2018-02-20 08:26:21 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,44 @@
 function basicTeenager(age) {
-
+  if (13<=age && age<=19) {
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if (13<=age && age<=19) {
+    return "You are a teenager!"
+  }
+  else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if (13<=age && age<=19) {
+    return "You are a teenager!"
+  }
+else if (13>age) {
+  return "You are a kid"
+}
+else {
+  return "You are a grownup"
+}
 }
 
 function ternaryTeenager(age) {
-
+return 13<=age && age<=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+switch (age) {
+  case 13<=age && age<=19:
+    return 'You are a teenager';
+    break;
+    case 13>age || age>19:
+      return 'You have an age';
+      break;
+  default:
+  return 'You have an age';
 
+}
 }
