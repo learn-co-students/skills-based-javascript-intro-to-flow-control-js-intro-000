4a53e1ff885e93664c02dd98a19915ca90f12741
tarheelnc83@outlook.com <tarheelnc83@outlook.com>
2018-02-28 20:09:31 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,52 @@
 function basicTeenager(age) {
-
+if(age>=13&&age<=19){
+    return("You are a teenager!")
+  }
 }
 
 function teenager(age) {
-
+  if(age>=13&&age<=19){
+      return("You are a teenager!")
+    }
+    else {
+      return("You are not a teenager")
+    }
 }
 
 function ageChecker(age) {
-
+  if(age>=13&&age<=19){
+      return("You are a teenager!")
+    }
+    else if(age<=12){
+      return("You are a kid")
+    }
+    else {
+      return("You are a grownup")
+    }
 }
 
 function ternaryTeenager(age) {
-
+return age>=13&&age<=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+switch (age) {
+  case age=13:
+  case age=14:
+  case age=15:
+  case age=16:
+  case age=17:
+  case age=18:
+  case age=19:
+    return("You are a teenager")
+    break;
+  default:
+  return("You have an age")
 
 }
+}
+
+basicTeenager(14)
+teenager(12)
+ageChecker(33)
+ternaryTeenager(18)
