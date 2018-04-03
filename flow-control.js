b88670bc9a6289467424bb3cba14404c6c5865d0
trevin <trevlivin@gmail.com>
2018-04-03 02:58:18 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -7,21 +7,21 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if(age <= 13 && age <= 19){
+  if(age >= 13 && age <= 19){
     return "You are a teenager!"
 
   }
-  else if(age > 19){
+  else if(age > 13 && age > 19){
     return "You are not a teenager"
   }
 
 }
 
 function ageChecker(age) {
-  if(age <=13 && age <= 19){
+  if(age >=13 && age <= 19){
     return "You are a teenager!"
   }
-  else if(age < 12){
+  else if(age <= 12){
     return "You are a kid"
   }
   else if(age > 19){
@@ -31,7 +31,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return age <= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
+  return age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
 
 }
 
