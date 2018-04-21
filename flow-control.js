c051501cf64f902feeda1448f8b7488e196f9296
jlindseygm6@gmail.com <jlindseygm6@gmail.com>
2018-04-21 12:58:23 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,55 @@
 function basicTeenager(age) {
 
+  if(age > 12 && age < 20){
+    return 'You are a teenager!'
+
+  }
+
 }
 
 function teenager(age) {
+  if(age > 12 && age < 20){
+    return 'You are a teenager!'
+
+  }
+  else {
+    return 'You are not a teenager'
+  }
 
 }
 
 function ageChecker(age) {
 
+  if(age > 12 && age < 20){
+    return 'You are a teenager!'
+
+  }
+  else if(age < 13) {
+    return 'You are a kid'
+  }
+  else if( age >19){
+    return 'You are a grownup'
+  }
+
 }
 
 function ternaryTeenager(age) {
 
+return (age > 12 && age < 20) ? 'You are a teenager':'You are not a teenager'
+
 }
 
 function switchAge(age) {
+
+  switch (age > 12 && age < 20){
+
+case true:
+  return 'You are a teenager'
+  break;
+  case false:
+  return 'You have an age'
+  break;
+
+  }
 
 }
