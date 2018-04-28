1d8b44b67c664a819e2083981447cb2f28bccc67
Anuj Pandey <pandey.anujk@gmail.com>
2018-04-28 21:29:55 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -14,11 +14,11 @@ function teenager(age) {
 
 function ageChecker(age) {
   if (age>=13 && age<= 19) {
-    return ("You are a teenager!")
+    return ("You are a teenager!");
     } else if (age<=12) {
-      return ("You are a kid")
+      return ("You are a kid");
     } else {
-      return ("You are a grownup")
+      return ("You are a grownup");
     }
 }
 
