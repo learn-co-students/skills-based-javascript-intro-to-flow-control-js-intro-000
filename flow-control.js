ea3b865b7af948c77a04fdd0adfbe118e9769f00
Carlos R <demo.ioverflow@gmail.com>
2018-05-23 23:24:59 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if ( age >=13 && age <= 19) {
+  if (age >=13 && age <= 19) {
     return "You are a teenager!";
   }else if (age < 13) {
     return "You are a kid";
