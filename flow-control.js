a3ea6f3103c4301dd51ae2b6277aa2eb0ff93d7c
Erika D'Auria <erika.dauria@gmail.com>
2018-05-31 01:19:47 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -27,9 +27,28 @@ else{
 }
 
 function ternaryTeenager(age) {
-
+return(age >= 13 && age <= 19)?
+"You are a teenager" :
+"You are not a teenager"
 }
 
 function switchAge(age) {
-
+switch(age) {
+  case 13:
+  return "You are a teenager"
+  case 14:
+  return "You are a teenager"
+  case 15:
+  return "You are a teenager"
+  case 16:
+  return "You are a teenager"
+  case 17:
+  return "You are a teenager"
+  case 18:
+  return "You are a teenager"
+  case 19:
+  return "You are a teenager"
+  default:
+  return "You have an age"
+}
 }
