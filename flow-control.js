b8e44cb21ce8d1420b19eb87a5ef8867d3698298
Cheryl Kelson <cherylkelson@icloud.com>
2018-06-18 20:38:00 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -27,25 +27,18 @@ function switchAge(age) {
   switch(age) {
     case 13:
       return ("You are a teenager");
-      break;
       case 14:
         return("You are a teenager");
-        break;
         case 15:
           return ("You are a teenager");
-          break;
           case 16:
             return ("You are a teenager");
-            break;
             case 17:
               return ("You are a teenager");
-              break;
               case 18:
                 return ("You are a teenager");
-                break;
                 case 19:
                   return ("You are a teenager");
-                  break;
                   default:
                   return ("You have an age");
 }
