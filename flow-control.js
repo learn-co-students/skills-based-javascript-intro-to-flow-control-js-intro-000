7d52e2b21235522de00cbb39af9812bd80674e29
Cheryl Kelson <cherylkelson@icloud.com>
2018-06-18 18:06:49 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -17,11 +17,12 @@ function ageChecker(age) {
     return ("You are a kid");
   } else {
     return ("You are a grownup");
+  }
 }
 function ternaryTeenager(age) {
   if (age >= 13 && age <= 19)
   return ("You are a teenager");
-} else {
+} else (age <= 13 && age <=19)
   return ("You are not a teenager");
 }
   }
