1658dab0ab9555d28d8137166b4563a7ddc9dcc7
Martin Cervantes <cervantes.martine@gmail.com>
2018-06-18 02:58:56 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -26,5 +26,18 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+    switch (age)
+    {
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+            return "You are a teenager";
+            break;
+        default:
+            return "You have an age";
+    }
 }
