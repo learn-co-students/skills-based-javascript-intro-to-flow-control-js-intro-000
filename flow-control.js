7188112681e58d4159f97bd4408f5b22cd609090
eggsvonsatan <elbadry.d@gmail.com>
2018-06-18 23:27:43 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,37 @@
+var age = 15
+
 function basicTeenager(age) {
-
+  if (age >= 13 && <= 19) {
+    return "You are a teenager!"
+  }
 }
+basicTeenager(15);
 
-function teenager(age) {
+function teenager(age) { if (age >= 13 && <= 19) {return "you are a teenager!"}
+else {return "you are not a teenager"}
 
-}
+};
 
-function ageChecker(age) {
+function ageChecker(age) { if (age >= 13 && <= 19) {return "you are a teenager!"}
+else if (age < 12) {return "you are a kid!"}
+else {return "you are a grownup"}
 
-}
+};
 
-function ternaryTeenager(age) {
+function ternaryTeenager(age) { return true
+  if (age >= 13 && <= 19 ) {return false}
 
 }
 
 function switchAge(age) {
+  var age = 12;
+  switch(age) {
+    case 13 || 14 || 15 || 16 || 17 || 18 || 19 :
+    return "you are a teenager";
+    break;
+    default:
+    return "you have an age";
+  }
+
 
 }
