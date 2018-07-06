4c27d179648858b9292aa56abfd7fc9d5ccebe18
Kanu Chinaemerem <obinnakans2003@gmail.com>
2018-07-06 00:57:44 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -9,19 +9,18 @@ function teenager(age) {
     return "You are a teenager!";
   }
     else {
-      return "You are not a teenager!";
+      return "You are not a teenager";
     }
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <=19) {
     return "You are a teenager!";
-  }
-    else if (age <=12) {
+  } else if (age <=12) {
       return "You are a kid";
-    }{
-      return "You are a grownup"
-    }
+  } else {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
@@ -29,10 +28,16 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch(age) {
-    case (age >= 13 && age <=19):
-      console.log("You are a teenager");
-      break;
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager";
     default:
-      console.log("You have an age");
+      return "You have an age";
+    }
 }
