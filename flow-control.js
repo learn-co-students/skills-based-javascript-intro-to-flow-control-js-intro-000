e683db456f038defbd03a3fd6a1683d0d70b69f3
Daniel James Stephenson <dan_0502@hotmail.co.uk>
2018-07-22 13:47:39 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -12,43 +12,42 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age >= 13 && age <= 19)
-  return ('You are a teenager!')
-  else if (true) {
-    (age <= 12)
-    return ('You are a kid')
+  if (age >= 13 && age <= 19) {
+  	return 'You are a teenager!'
+    }
+  else if (age <= 12) {
+    return 'You are a kid'
   }
   else {
-    (age >= 20)
-    return ('You are a grownup')
+    return 'You are a grownup'
   }
 }
 
 function ternaryTeenager(age) {
-  return age ? 'You are a teenager' : 'You are not a teenager'
+  return (age >= 13 && age <= 19) ? 'You are a teenager' : 'You are not a teenager'
 }
 
 function switchAge(age) {
 switch (age) {
-  case "13":
+  case 13:
     return ('You are a teenager')
     break;
-    case "14":
+    case 14:
       return ('You are a teenager')
       break;
-      case "15":
+      case 15:
         return ('You are a teenager')
         break;
-        case "16":
+        case 16:
           return ('You are a teenager')
           break;
-          case "17":
+          case 17:
             return ('You are a teenager')
             break;
-            case "18":
+            case 18:
               return ('You are a teenager')
               break;
-              case "19":
+              case 19:
                 return ('You are a teenager')
   default:
   return ('You have an age')
