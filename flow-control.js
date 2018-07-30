668ca67131c83f13829bee30b177c2712ca850b4
Andrea Abshire <sashady1488@gmail.com>
2018-07-30 16:18:54 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -11,7 +11,7 @@ function teenager(age) {
      return "You are not a teenager";
 
 }
-
+}
 function ageChecker(age) {
     if (age >=13 && age <= 19) {
       return "You are a teenager!"
@@ -27,5 +27,31 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
+   switch(age) {
+     case 13:
+     return "You are a teenager";
+     break;
+     case 14:
+     return "You are a teenager";
+     break;
+     case 15:
+     return "You are a teenager";
+     break;
+     case 16:
+     return "You are a teenager";
+     break;
+     case 17:
+     return "You are a teenager";
+     break;
+     case 18:
+     return "You are a teenager";
+     break;
+     case 19:
+     return "You are a teenager";
+     break;
+     default:
+     return "You have an age";
+   
+   }
 
 }
