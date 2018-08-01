9c7c14ea1c802005868aad5dc47435b60ef1cd9b
Christa Gabert <gabert.cm@gmail.com>
2018-08-01 22:21:54 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -18,16 +18,25 @@ function ageChecker(age) {
   if (age >= 13 && age <= 19) {
   return 'You are a teenager!'
 } else if (age < 13) {
-  return 'You are not a kid'
+  return 'You are a kid'
 } else if (age > 19) {
   return 'You are a grownup'
 }
 }
 
 function ternaryTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+  return 'You are a teenager'
+} else if (age < 13 || age > 19) {
+  return 'You are not a teenager'
+}
 }
 
-function switchAge(age) {
 
+function switchAge(age) {
+  if (age >= 13 && age <= 19) {
+  return 'You are a teenager'
+} else if (age < 13 || age > 19) {
+  return 'You have an age'
+}
 }
