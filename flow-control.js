3294b50a2b1b43c90c11c4876aa191b1aa1a4d02
Bushra Masud <bushra.masud111@gmail.com>
2018-08-19 06:49:30 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -28,7 +28,7 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   if(age>=13 && age<=19){
-    return("You are a teenager!")
+    return("You are a teenager")
   }
 
   return("You are not a teenager")
@@ -37,12 +37,29 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case "age>=13 && age<=19":
+    case 13:
     return "You are a teenager"
-  }
-
-      break;
+    break;
+    case 14:
+    return "You are a teenager"
+    break;
+    case 15:
+    return "You are a teenager"
+    break;
+    case 16:
+    return "You are a teenager"
+    break;
+    case 17:
+    return "You are a teenager"
+    break;
+    case 18:
+    return "You are a teenager"
+    break;
+    case 19:
+    return "You are a teenager"
+    break;
     default:
     return "You have an age"
+  }
 
   }
