6d8cf22b27c09409199e210c2d95619347c407ce
Michael Hefner <michaelphefner@gmail.com>
2018-11-03 18:40:24 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,48 @@
-function basicTeenager(age) {
+teen = 'You are a teenager';
+notTeen = 'You are not a teenager';
+kid = 'You are a kid';
+grownup = 'You are a grownup';
+haveAge = 'You have an age';
 
+function basicTeenager(age) {
+  if(age > 12 && age < 20){ return teen + '!'; }
 }
 
 function teenager(age) {
-
+  return (age > 12 && age < 20) ? (teen + '!') : notTeen;
 }
 
 function ageChecker(age) {
-
+  if( age > 12 && age < 20){
+    return teen + '!';
+  }else if (age < 13) {
+    return kid;
+  }else{
+    return grownup;
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return (age > 12 && age < 20) ? teen : notTeen;
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+      return teen
+    case 14:
+      return teen
+    case 15:
+      return teen
+    case 16:
+      return teen
+    case 17:
+      return teen
+    case 18:
+      return teen
+    case 19:
+      return teen
+    default:
+      return haveAge
+  }
 }
