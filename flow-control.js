bafb5d6d8746540c2a9026576183a16a66bc373c
brave-course-1385 <tania@pixelsandcode.studio>
2018-11-06 19:43:40 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -11,14 +11,15 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
-
-}
+  if (age >= 13 && age <= 19){
+   return "You are a teenager!"
+  }
+  }
 
 function ternaryTeenager(age) {
 
-}
+  }
 
 function switchAge(age) {
 
-}
+  }
