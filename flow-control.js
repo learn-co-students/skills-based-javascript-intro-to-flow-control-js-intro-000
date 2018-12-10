85ac0d37f053a2899d22a03b1b464fa6751bf3f2
Yosuf Gurung <yosufgurung@gmail.com>
2018-12-10 22:11:26 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -5,17 +5,40 @@ if(13 <= age && age <= 19) {
 }
 
 function teenager(age) {
-
+if(13 <= age && age <= 19) {
+  return 'You are a teenager!'
+} else {
+  (13 > age && age >19)
+  return  'You are not a teenager'
+}
 }
 
 function ageChecker(age) {
-
+  if(13 <= age && age <= 19) {
+    return 'You are a teenager!'
+  } else if (age <= 12) {
+    return 'You are a kid'
+  } else if (20 <= age) {
+    return 'You are a grownup'
+}
 }
 
 function ternaryTeenager(age) {
+    return (13 <= age && age <= 19) ? 'You are a teenager' : 'You are not a teenager'
+  }
 
-}
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return 'You are a teenager'
+    default:
+      return 'You have an age'
+  }
 }
