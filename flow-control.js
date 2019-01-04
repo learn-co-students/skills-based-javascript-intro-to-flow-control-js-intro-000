d6900f8bd0bdb560a883815a62473d23e010fa36
Christina Dong <christinadongla@yahoo.com>
2019-01-04 21:15:29 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,55 +1,56 @@
 function basicTeenager(age) {
-  if (age >=13 && age <=19)
-    {return "You are a teenager!";
+  if (age >=13 && 19>= age) {
+    return "You are a teenager!";
   }
 }
 
+
 function teenager(age) {
-  if (13<= age && age <=19) {
-    console.log("You are a teenager!")
+  if (age >=13 && age <= 19) {
+    return"You are a teenager!";
   } else {
-    return"You are not a teenager"
+    return"You are not a teenager";
   }
 }
 
 function ageChecker(age) {
-  if (13<= age && age <=19) {
-    console.log("You are a teenager!")
-    else if (12>age) {
-      console.log("You are a kid ")
-      else if (age>19) {
-        return"You are a grownup"
+  if (age >=13 && age <=19) {
+    return"You are a teenager!";
+  } else if (age <=12) {
+      return"You are a kid";
+    } else if (age >19) {
+        return"You are a grownup";
       }
 }
 
 function ternaryTeenager(age) {
-return  13<= age && age <=19 ? "You are a teenager" : "You are not a teenager"
+return  age >=13 && age <=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
   switch(age) {
     case 13:
-      console.log("You are a teenager");
+      return"You are a teenager";
       break;
     case 14:
-      console.log("You are a teenager");
+      return"You are a teenager";
       break;
     case 15:
-      console.log("You are a teenager");
+      return"You are a teenager";
       break;
     case 16:
-      console.log("You are a teenager");
+      return"You are a teenager";
       break;
     case 17:
-      console.log("You are a teenager");
+      return"You are a teenager";
       break;
     case 18:
-      console.log("You are a teenager");
+      return"You are a teenager";
       break;
     case 19:
-      console.log("You are a teenager");
+      return"You are a teenager";
       break;
     default:
-    return("You have an age");
+      return"You have an age";
   }
 }
