fd71a4e49ad62ab8af508381c614476f6e46c759
Senai Mogos <smogos797@gmail.com>
2019-01-07 04:20:40 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,40 @@
+
 function basicTeenager(age) {
+  if (age == 13-19)
+  return true
+    print('you are a teenager')
+
 
 }
 
 function teenager(age) {
-
+  if (age == 13-19)
+    print('you are a teenager')
+  else:
+    print('you are not a teenager')
 }
 
 function ageChecker(age) {
-
+  if (age == 13-19)
+    print('you are a teenager')
+  elif (age < 12):
+    print('You are a kid')
+  elif (age > 19)
+    print('you are a grownup')
 }
 
 function ternaryTeenager(age) {
-
+  if (age == 13-19)
+    print('you are a teenager')
+  return false
+    print('you are not a teenager')
 }
 
 function switchAge(age) {
+  switch(age) {
+    case "13":
+  }
 
 }
+
+if age
