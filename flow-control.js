ee33fc4b82e5f8aba6fc60f8845d98d26ed9523d
Theodore Kimana <kimana.theodore@gmail.com>
2019-01-08 02:39:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -8,20 +8,22 @@ function basicTeenager(age) {
 function teenager(age) {
   if(age>=13 && age<=19){
     return "You are a teenager!"
-  }
-
 }
 
 function ageChecker(age) {
   if(age<=12){
   return "You are a kid"
  }
+  if(age>=13 && age<=19){
+  return "You are a teenager!"
+ }
   if (age>=20){
     return "You are a grownup"
+
  }
 }
 
-}
+
 
 function ternaryTeenager(age) {
   return age >=13 && age <= 19 ?
