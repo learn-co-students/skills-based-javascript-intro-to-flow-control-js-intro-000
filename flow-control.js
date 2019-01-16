b4c3aa9bc8aeecee38aff2f7d2e9a82ada441d9c
Konstantin Grachev <kgrachev@gmail.com>
2019-01-16 17:48:34 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,40 @@
-function basicTeenager(age) {
+console.log("PLEASE LOG SOMETHING FOR ME");
 
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  } else {
+    return undefined;
+  }
 }
 
+
+
 function teenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
+  else return "You are not a teenager";
 
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  } else if (age<13) return "You are a kid"
+  else return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
-
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
-
+  switch (age >= 13 && age <= 19) {
+    case true: return "You are a teenager"
+      break;
+    case false: return "You have an age"
+      break;
+    default:
+  }
 }
