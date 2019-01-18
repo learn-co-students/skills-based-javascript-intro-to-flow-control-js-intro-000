482ea50b292dff873e684778ad12c511a7635eb9
Maxi Sagmeister <sagmeister@gmail.com>
2019-01-18 00:58:09 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,26 @@
 function basicTeenager(age) {
-
+  if (13 <= basicTeenager && basicTeenager <= 19) {
+    return("You are a teenager!")
+  }
 }
 
 function teenager(age) {
-
+  if (13 <= basicTeenager <= 19) {
+    return ("You are a teenager!")
+  } else {
+    return("You are not a teenager!")
+  }
 }
 
 function ageChecker(age) {
-
+  if (13 <= basicTeenager <= 19) {
+    return ("You are a teenager!")
+  } else if (basicTeenager =< 12) {
+    return ("You are a kid")
+  }
+  else {
+    return ("You are a grownup.")
+  }
 }
 
 function ternaryTeenager(age) {
