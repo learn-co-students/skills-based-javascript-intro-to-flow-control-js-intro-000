9ced9666fcd5bd2b179912e0e4926d25b70f7ffc
interpreted-number-7402 <jxa428@psu.edu>
2019-01-27 08:36:47 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,13 @@
 function basicTeenager(age) {
-
+  if (age >=13 && age <=19) {
+    return "You are a teenager!"
+  }
 }
-
 function teenager(age) {
-
-}
+    else {
+      return "You are not a teenager!"
+    }
+  }
 
 function ageChecker(age) {
 
