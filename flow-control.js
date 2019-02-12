a2b7fe1fe86381c6be6406901164740ad96f079d
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-12 22:32:04 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,55 @@
 function basicTeenager(age) {
-
+  if(age >= 13 && age <= 19){
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if(age >= 13 && age <= 19){
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if(age >= 13 && age <= 19){
+    return "You are a teenager!"
+  } else if (age <= 12){
+    return "You are a kid"
+  } else if (age > 20){
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-
+ return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch(age){
+    case 13:
+      console.log("You are a teenager")
+      break;
+    case 14:
+      console.log("You are a teenager")
+      break;
+    case 15:
+      return "You are a teenager"
+      break;
+    case 16:
+      console.log("You are a teenager")
+      break;
+    case 17:
+      console.log("You are a teenager")
+      break;
+    case 18:
+      console.log("You are a teenager")
+      break;
+    case 19:
+      console.log("You are a teenager")
+      break;
+    default:
+      return"You have an age";
+  }
 }
