32fa3331c98428fc2a4d79d8289213bce93c462d
Natasha Morales <nath00@hotmail.com>
2019-02-16 15:29:11 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,23 @@
 function basicTeenager(age) {
-
+  if (13 <= 19)
+  return {"You are a teenager!" }
+ else (13 > 0 || 100 > 19)
+ return {'You are not a teenager'}
 }
 
 function teenager(age) {
-
+ if (13 <= 19)
+ return {"you are a teenager!"}
+ else (0 <= 12 || 19 <= 150)
+ return {"You are not a teenager"}
 }
 
 function ageChecker(age) {
-
+ if (0 <= 12)
+ return {"You are a kid!"}
+ else if (13 <= 19)
+ return {"You are a teenager!"}
+ else ()
 }
 
 function ternaryTeenager(age) {
