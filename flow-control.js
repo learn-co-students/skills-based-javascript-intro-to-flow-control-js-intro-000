29b71c906cd77ec3eb3a05960e4aa065a91db5e1
Emir Cajic <emircajic@yahoo.com>
2019-02-21 21:49:38 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,48 @@
 function basicTeenager(age) {
 
+if(age>12&&age<20){
+  return 'You are a teenager!'
+}
+return undefined
 }
 
 function teenager(age) {
+  var string="You are not a teenager"
 
+  if(age>12&&age<20){
+    return 'You are a teenager!'
+  }
+  return "You are not a teenager"
 }
 
 function ageChecker(age) {
 
+  if(age>12&&age<20){
+    return 'You are a teenager!'
+  }
+
+  else if (age<13) {
+      return "You are a kid"
+  }
+
+  else {
+      return "You are a grownup"
+  }
+
 }
 
 function ternaryTeenager(age) {
+  var string="You are not a teenager"
 
+  if(age>12&&age<20){
+    return 'You are a teenager'
+  }
+  return "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  if(age>12&&age<20){
+    return 'You are a teenager'
+  }
+  return "You have an age"
 }
