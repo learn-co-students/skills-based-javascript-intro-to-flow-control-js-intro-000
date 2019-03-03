d7d79d1e5b170a9a473af546de205f7ead4b5750
Chloe Liu <geyiliu@gmail.com>
2019-03-03 02:32:30 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -41,29 +41,32 @@ function ternaryTeenager(age) {
 
 // Define a function switchAge that accepts an age as a parameter. The case statement should switch on age and
 // return "You are a teenager" if the age is 13, 14, 15, 16, 17, 18, or 19, and return "You have an age" as the default.
-// function switchAge(age) {
-//
-//   case 13:
-//     return 'You are a teenager';
-//
-//   case 14:
-//     return 'You are a teenager';
-//
-//   case 15:
-//     return 'You are a teenager';
-//
-//   case 16:
-//     return 'You are a teenager';
-//
-//   case 17:
-//     return 'You are a teenager';
-//
-//   case 18:
-//     return 'You are a teenager';
-//
-//   case 19:
-//     return 'You are a teenager';
-//
-//   default:
-//     return "You have an age";
-// }
+function switchAge(age) {
+
+  switch(age) {
+    case 13:
+      return 'You are a teenager';
+
+    case 14:
+      return 'You are a teenager';
+
+    case 15:
+      return 'You are a teenager';
+
+    case 16:
+      return 'You are a teenager';
+
+    case 17:
+      return 'You are a teenager';
+
+    case 18:
+      return 'You are a teenager';
+
+    case 19:
+      return 'You are a teenager';
+
+    default:
+      return "You have an age";
+  }
+
+}
