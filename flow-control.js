8c4eb3bc16c2b9d1a9fdccb8fd46ba8aee284a80
vivienne Owusu-Ansah <voa2000@hotmail.com>
2019-03-13 11:48:40 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -27,13 +27,17 @@ return (age >= 13 && age <= 19) ?
 "You are not a teenager";
 }
 
-function switchAge(age) {
-switch (age) { 
-  case age >= 13 && age <= 19:
-return "You are a teenager";
-//break;
-default:
-  return "You have an age";
-}
-  console.log("You have an age");
+function switchAge(age){
+  switch (age) {
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+    return "You are a teenager";
+  default:
+    return "You have an age"
+  }
 }
