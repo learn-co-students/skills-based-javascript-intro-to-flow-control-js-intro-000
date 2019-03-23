2d27cef0f219cc2a3d0f4b1c7f2e494a38230e3d
Evan LaRusso <lalalarusso@gmail.com>
2019-03-23 16:12:55 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,21 +1,36 @@
+if(conditionToTest) { // executed if 'conditionToTest' is truthy
+} else { // executed if 'conditionToTest' is falsey
+}
+
 function basicTeenager(age) {
     if(age >= 13 && age <= 19) {
-      // executed if 'conditionToTest' is truthy
-       return "You are a teenager!"
+       return 'You are a teenager!'
      }
   }
 
 function teenager(age) {
     if(age >= 13 && age <= 19) {
-      // executed if 'conditionToTest' is truthy
        return "You are a teenager!"
     } else {  
-      // executed if 'conditionToTest' is falsey } 
-          return "You are not a teenager"   /// here
-    } 
+          return "You are not a teenager"
+    }
+    
   }
   
-function ageChecker(age) {
+function teenager(age) { 
+
+}
+
+function ageChecker(age) { 
+  if (age >= 13 && age <= 19) {
+       return "You are a teenager!"
+} else if (age <= 12) {
+  // execute this code if `(age >= 13 && age <= 19)` statement is falsey AND `(age <= 12)` is truthy
+  return "You are a kid"
+} else { 
+  return "You are a grownup"
+  // execute this code if none of the other conditions are met
+}
 
 }
 
