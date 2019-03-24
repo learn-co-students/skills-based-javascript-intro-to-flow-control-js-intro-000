b64fbacc02c50ca4ec0e19397fc0bfb4af630f6e
Evan LaRusso <lalalarusso@gmail.com>
2019-03-24 15:48:17 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -31,7 +31,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-return (age >= 13 && <= 19 ? "You are a teenager" : "You are not a teenager");
+return age >= 13 && <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
