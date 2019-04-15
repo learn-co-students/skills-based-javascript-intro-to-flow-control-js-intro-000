0f825467d4be3ceec6df7c75af6f524fd6df83a5
Brad Ayers <brad@bradayers.com>
2019-04-15 23:47:23 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,52 @@
 function basicTeenager(age) {
-
+  if (age>12 && age< 20) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
+  if (age>12 && age< 20) {
+    return "You are a teenager!";
+  }
+  else {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
-
+  if (age < 13) {
+    return "You are a kid";
+  }
+  else if (age > 20){
+    return "You are a grownup";
+  }
+  else {
+    return "You are a teenager!";
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return (age>12 && age< 20) ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
-
+var teenager = "You are a teenager";
+switch (age) {
+  case 13 :
+  return teenager;
+  case 14 :
+  return teenager;
+  case 15 :
+  return teenager;
+  case 16 :
+  return teenager;
+  case 17 :
+  return teenager;
+  case 18 :
+  return teenager;
+  case 19 :
+  return teenager;
+  default :
+  return "You have an age";
+}
 }
