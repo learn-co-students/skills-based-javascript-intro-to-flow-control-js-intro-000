635498ccb1ba77e72ba49f40c93bc59e0f1fe3d2
Giovanny <giovannydiaz98@yahoo.com>
2019-04-21 00:44:12 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -33,13 +33,13 @@ switch(age){
       case 15:{
         return "You are a teenager";}
         case 16:{
-          return "You area a teenager";}
+          return "You are a teenager";}
           case 17:{
           return "You are a teenager";}
            case 18:{
            return "You are a teenager";}
            case 19:{
-             return "Uou are a teenager";}
+             return "You are a teenager";}
       break;
    default:
  return("You have an age");
