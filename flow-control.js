95c9a56d0a3a687f63e4e4283dad86934620e552
Rezwan Mazumder <rezwanmazumder@gmail.com>
2019-04-30 00:14:41 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,14 @@
-function basicTeenager(age) {
+function basicTeenager(age) { if (age>=13 && age<=19) {
+  return "You are a teenager!"}
+
 
 }
 
-function teenager(age) {
+function teenager(age) { if (age>=13 && age<=19) {
+return "You are a teenager"
+else {
+  return "You are not a teenager"
+}
 
 }
 
