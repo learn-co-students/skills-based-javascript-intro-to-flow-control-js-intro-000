6fa65f434120a21bc2908866a06b5b0ca9c8cdf8
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-14 10:07:41 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,27 +1,33 @@
 function basicTeenager(age) {
-if (age >= 13 && age <= 19) {
+  if (age >= 13 && age <= 19){
   return "You are a teenager!";
-  }
-  }
+}
+}
 
 function teenager(age) {
-if (age >= 13 && age <= 19) {
-return "You are a teenager!";
-} else {return "You are not a teenager";}
+  if (age >= 13 && age <= 19){
+  return "You are a teenager!";
+} else {
+  return "You are not a teenager";
+}
 }
 
-function ageChecker(age)  {
-if (age >= 13 && age <= 19) {
-return "You are a teenager!";
-} else if (age <= 12) {
-return "You are a kid";
-} else {return "You are a grownup";}
-}
+function ageChecker(age) {
+   if (age >= 13 && age <= 19){
+  return "You are a teenager!";
+   } else if (age <= 12){
+     return "You are a kid";
+   } else if (age > 19){
+     return "You are a grownup";
+   }
+   }
 
 function ternaryTeenager(age) {
-if (age >= 13 && age <= 19) {
-return "You are a teenager";
-} else {return "You are not a teenager";}
+  if (age >= 13 && age <= 19){
+  return "You are a teenager";
+} else {
+  return "You are not a teenager";
+}
 }
 
 function switchAge(age) {
