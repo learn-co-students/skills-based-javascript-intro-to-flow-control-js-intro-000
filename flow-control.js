abd4dac0ecdafe5093073d9b65c2866a27737631
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-05-22 22:14:58 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -8,7 +8,9 @@ function teenager(age) {
   if (13 <= age <= 19) {
     return "You are a teenager!";
   }
-  return "You are not a teenager";
+  else {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
@@ -18,11 +20,13 @@ function ageChecker(age) {
   else if (age <= 12) {
     return "You are a kid";
   }
-  return "You are a grownup";
+  else {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
-  13 <= age <= 19 ? return "You are a teenager" : return "You are not a teenager";
+  return 13 <= age <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
