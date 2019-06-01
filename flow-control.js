cd3abf9f18d5de4bcbc7d580446667d19e28cc61
Juliette Cabrera <julsc256@yahoo.com>
2019-06-01 00:35:10 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -19,7 +19,34 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
+  age > 13 && age < 19 ? "You are a teenager" :
+  "You are not a teenager"
 }
 
 function switchAge(age) {
+  switch(age){
+    case "13":
+    return "You are a teenager";
+    break;
+    case "14":
+    return "You are a teenager";
+    break;
+    case "15":
+    return "You are a teenager";
+    break;
+    case "16":
+    return "You are a teenager";
+    break;
+    case "17":
+    return "You are a teenager";
+    break;
+    case "18":
+    return "You are a teenager";
+    break;
+    case "19":
+    return "You are a teenager";
+    break;
+    default:
+    return "You have an age";
+  }
 }
