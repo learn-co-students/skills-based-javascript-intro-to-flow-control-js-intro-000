4bc7fb2fa2c4caa2ba704ea0ac1b90e6eff18e47
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-04 00:13:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,28 @@
-function basicTeenager(age) {
-
+function basicTeenager(age) {if (age >= 13 && age <= 19) {
+return "You are a teenager!";
+}
 }
 
-function teenager(age) {
-
+function teenager(age) {if (age>=13 && age <= 19){
+return "You are a teenager!";
+} else {return "You are not a teenager";}
 }
 
-function ageChecker(age) {
-
+function ageChecker(age) {if (age>=13 && age <= 19){
+return "You are a teenager!";
+} else if (age <= 12){
+return "You are a kid";
+} else {return "You are a grownup";
+}
 }
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager(age) {if (age >= 13 && age <= 19){
+return "You are a teenager";
+} else {return "You are not a teenager";}
 }
 
-function switchAge(age) {
-
+function switchAge(age) {if (age >= 13 && age <= 19) {
+return "You are a teenager";
+} else { (age > 19 && age < 13)
+return "You have an age";}
 }
