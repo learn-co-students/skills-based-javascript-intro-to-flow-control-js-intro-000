d981db07c099b8ec467c171ec8fd98af8cc61976
Joshua Choi <jsc.r839@gmail.com>
2019-06-06 22:11:18 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,59 @@
-function basicTeenager(age) {
-
+var basicTeenager = function(age) {
+  if (age >= 13 && age <= 19 ) {
+    return "You are a teenager!"
+  }
 }
 
-function teenager(age) {
-
+var teenager = function(age) {
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!";
+  } else{
+    return "You are not a teenager";
+  }
 }
 
-function ageChecker(age) {
-
+var ageChecker = function(age) {
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!";
+  } else if (age <= 12) {
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
 }
 
-function ternaryTeenager(age) {
-
+var ternaryTeenager = function(age) {
+  if (age >= 13 && age <= 19){
+    return "You are a teenager";
+  } else {
+    return "You are not a teenager";
+  }
 }
 
-function switchAge(age) {
-
+var switchAge = function(age) {
+  switch (age) {
+    case 13:
+      return "You are a teenager";
+      break;
+    case 14:
+      return "You are a teenager";
+      break;
+    case 15:
+      return "You are a teenager";
+      break;
+    case 16:
+      return "You are a teenager";
+      break;
+    case 17:
+      return "You are a teenager";
+      break;
+    case 18:
+      return "You are a teenager";
+      break;
+    case 19:
+      return "You are a teenager";
+      break;
+    default:
+      return "You have an age";
+  }
 }
