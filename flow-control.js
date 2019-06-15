e70bc7d7f326adc2d9c69fcc38eec22fdd59af9d
Stephanie Ma <stephanieoma@gmail.com>
2019-06-15 20:39:04 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,16 +1,32 @@
 function basicTeenager(age) {
-
+if (age <= 13 && 19 >= age) {
+  return "You are a teenager!"
+}
 }
 
 function teenager(age) {
-
+if (age >= 13 && 19 <= age) {
+  return "You are a teenager!"
+}
+else if (age < 13 && 19 < age) {
+  return "You are not a teenager"
+}
 }
 
 function ageChecker(age) {
-
+if (age >= 13 && 19 <= age) {
+  return "You are a teenager!"
+}
+else if (age < 12) {
+  return "You are a kid"
+}
+else (age > 19) {
+  return "You are a grownup"
+}
 }
 
 function ternaryTeenager(age) {
+ternaryTeenager ?
 
 }
 
