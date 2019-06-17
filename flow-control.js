68598d85a6f87cae8ff1166608bcddbcb7a99a96
Jay Dong <jdong135@gmail.com>
2019-06-17 14:45:30 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,44 @@
 function basicTeenager(age) {
-
+  if(age>=13 && age<=19){
+    return('You are a teenager!');
+  }
 }
 
 function teenager(age) {
-
+  if(age>=13 && age<=19){
+    return('You are a teenager!');
+  }
+  else{
+    return('You are not a teenager');
+  }
 }
 
 function ageChecker(age) {
-
+  if(age>=13 && age<=19){
+    return('You are a teenager!');
+  }
+  else if(age<=12){
+    return('You are a kid');
+  }
+  else if(age>=20){
+    return('You are a grownup');
+  }
 }
 
 function ternaryTeenager(age) {
-
+  if(age>=13 && age<=19){
+    return('You are a teenager!');
+  }
+  else{
+    return('You are not a teenager');
+  }
 }
 
 function switchAge(age) {
-
+  case (age>=13 && age<=19):
+    return('You are a teenager');
+    break;
+  case (age<13 || age>19):
+    return('You have an age');
+    break;
 }
