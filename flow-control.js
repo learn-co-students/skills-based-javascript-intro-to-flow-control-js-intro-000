c04fbb2e2aeebcc04275cdcc1f922251a8f32ac7
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-06-29 22:39:39 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,38 @@
 function basicTeenager(age) {
+ var x = age;
+ if (x > 12 && x < 20) {
+   return "You are a teenager!"}
 
 }
 
 function teenager(age) {
-
+ var x = age;
+ if (x > 12 && x < 20) {
+   return "You are a teenager!"}
+  else {return "You are not a teenager"}
 }
 
 function ageChecker(age) {
+   var x = age;
+ if (x > 12 && x < 20) {
+   return "You are a teenager!"}
+  else if (x <= 12) {
+    return "You are a kid"}
+    else return "You are a grownup"
 
 }
 
 function ternaryTeenager(age) {
-
+ var x = age;
+ if (x > 12 && x < 20) {
+   return "You are a teenager"}
+  else {return "You are not a teenager"}
 }
 
 function switchAge(age) {
+   var x = age;
+ if (x > 12 && x < 20) {
+   return "You are a teenager"}
+  else {return "You have an age"}
 
 }
