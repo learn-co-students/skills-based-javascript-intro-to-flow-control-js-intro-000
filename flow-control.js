98f6b3f891eb78cadd5618b9a30fb3b66c1ba62d
E.B <edenbenbow@yahoo.com>
2019-07-04 02:47:45 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,53 @@
-function basicTeenager(age) {
+function basicTeenager(age){
+  if (age>12 && age<20) {
+    return"You are a teenager!"
+  }
+}
+
+function teenager(age){
+  if (age>12 && age<20) {
+    return"You are a teenager!"
+
+  } else {
+    return"You are not a teenager"
+  }
 
 }
 
-function teenager(age) {
+
+function ageChecker(age){
+  if (age>12 && age<20) {
+    return"You are a teenager!"
+} else if (age<13) {
+  return"You are a kid"
+}
+
+else {
+    return"You are a grownup"
+  }
 
 }
 
-function ageChecker(age) {
 
+
+function ternaryTeenager(age){
+  return (age>12 && age<20) ? "You are a teenager" : "You are not a teenager"
 }
 
-function ternaryTeenager(age) {
+function switchAge(age){
 
+
+switch (age) {
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+  return("You are a teenager")
+  break;
+  default:
+  return("You have an age")
 }
-
-function switchAge(age) {
-
 }
