b7cd9fda8e6c0053b7402b7988f90c94dc974daf
energetic-port-0799 <m.tarrats@yahoo.com>
2019-07-10 01:12:06 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,51 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager!"
+  }
+  return
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager!"
+  }
+  else if (age <= 12) {
+    return "You are not a teenager"
+  }
+  else if (age >= 29) {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager!"
+  }
+  else if (age <= 12) {
+    return "You are a kid"
+  }
+  else if (age >= 29) {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager"
+  }
+  else if (75) {
+  return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
-
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager"
+  }
+  else if (75) {
+    return "You have an age"
+  }
+  else if (7) {
+    return "You have an age"
+  }
 }
