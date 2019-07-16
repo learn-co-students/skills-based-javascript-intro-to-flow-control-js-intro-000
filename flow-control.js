ac2ae78c59f5f10794c7dbce22fe641d5dd244e3
voluble-block-7446 <iagogomes80@gmail.com>
2019-07-16 20:50:43 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,47 @@
 function basicTeenager(age) {
-
+  if(age>=13 && age<=19){
+    return "You are a teenager!";
+  }
+  else{
+    return undefined;
+  }
 }
 
 function teenager(age) {
-
+  if(age>=13 && age<=19){
+    return "You are a teenager!";
+  }
+  else{
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
-
+  if(age>=13 && age<=19){
+    return "You are a teenager!";
+  }
+  else if(age<13){
+    return "You are a kid";
+  }
+  else if(age>19){
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
+  return (age>=13 && age<=19)?  "You are a teenager" : "You are not a teenager";
 
 }
 
 function switchAge(age) {
+  switch(true){
+    case (age>=13 && age<=19):
+      return "You are a teenager";
+      break;
+    case(age<13 || age>19):
+      return "You have an age";
+      break;
+      
+  }
 
 }
