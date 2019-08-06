551eb69e9aa2835cf2e44e166fe9f020c9be70ac
Abdul Hannan Tahir <a.hannantahir@gmail.com>
2019-08-06 15:31:41 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,35 +1,43 @@
 function basicTeenager(age) {
 if (age>=13 && age<=19) {
-return You are a teenager!;
+return "You are a teenager!";
 }
 }
 
 function teenager(age) {
-  if (age>=13 && age<=19)
-  "You are a teenager!"
-  else ()
-  "You are not a teenager!"
+  if (age>=13 && age<=19) {
+  return "You are a teenager!" }
+  else {
+  return "You are not a teenager"
+}
 }
 
 function ageChecker(age) {
-  if (age>=13 && age<=19)
-  "You are a teenager!"
-  else if (age<13)
-  "You are a kid!"
-  else
-    "You are a grownup"
+  if (age>=13 && age<=19) {
+  return "You are a teenager!" }
+  else if (age<13) {
+  return "You are a kid" }
+  else {
+    return "You are a grownup"
   }
 }
 
 function ternaryTeenager(age) {
-age>=13 && age<=19 ? "You are a teenager!" : "You are not a teenager!"
+return age>=13 && age<=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-  witch(age) {
-    case 13, 14, 15, 16, 17, 18, 19:
-      console.log("You are a teenager");
+  switch(age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager";
       break;
       default:
-      console.log("You have an age")
+      return "You have an age"
+    }
 }
