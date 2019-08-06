76f5251c6cd609dbcb684d29bd3b9732ef222a5d
Amanda R <abrolon87@gmail.com>
2019-08-06 18:42:01 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -21,16 +21,15 @@ describe('flow-control', () => {
     })
   })
 
-  describe('basicTeenager', () => {
-    it('should return "You are a teenager!" if the age is between 13-19', () => {
+  function basicTeenager(age) {
+    if (age >= 13 && age <= 19) return "You are a teenager!"
       expect(basicTeenager(13)).toEqual("You are a teenager!");
-    })
+    }
 
-    it('should return undefined if the age is not between 13-19', () => {
+    if('should return undefined if the age is not between 13-19', () => {
       expect(basicTeenager(12)).toBe(undefined)
     })
 
-  })
 
   describe('teenager', () => {
     it('should return "You are a teenager!" if the age is between 13-19', () => {
