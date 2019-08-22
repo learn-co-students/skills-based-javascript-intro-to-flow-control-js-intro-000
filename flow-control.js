d19ed3c96b0fbe020d28796adf91fce6d20f3ed2
Abdulbaasit <baasisek01@gmail.com>
2019-08-22 16:33:35 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -30,5 +30,11 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+switch (age) {
+  case "age>=13 && age<=19":
+    return `You are a teenager`
+    break;
+  default:
+    return `You have an age`
+}
   }
