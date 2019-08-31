bce997e954ecf62769e06d4d5324cd241fcf1738
Melike KILIC <melike180400@gmail.com>
2019-08-31 04:46:41 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,43 +1,13 @@
-function  basicTeenager(15);
-    if(age>===13||age<===19){
-    return("You are a teenager!");
-  }}
+function  basicTeenager(age){
 
-
-
-
+    if(age >= 13 && age <= 19){
+    return "You are a teenager!";
+  }
 }
 function teenager(age) {
-  if(age >= 13 || age =< 19){
-    return("You are a teenager!")
+  if(age >= 13 && age =< 19){
+    return "You are a teenager!"
   }else{
-    return("You are not a teenager")
+    return "You are not a teenager"
   }
-    }
-}
-
-function ageChecker(age) {
-if(age===13-19){
-  return("You are a teenager!")
-}else if(age<===12){
-  return("You are a kid")
-}else if(age>19){
-  return("You are a grownup")
-}
-
-}
-
-function ternaryTeenager(age) {
-  if(age===13-19){
-    return("You are a teenager")
-}else {
-  return("You are not a teenager")}
-
-
-function switchAge(age) {
-switch(age){
-  case (age<= 19 && age=> 13):
-  return "You are a teenager"
-  default:
-}
 }
