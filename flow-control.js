e5a4fa1b3facc58fb9089f2db9dc4433bd1668a8
Denis Belioglo <belioglo@gmail.com>
2019-09-25 00:52:12 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -28,7 +28,13 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
    switch(age) {
-     case 15 :
+     case 13:
+     case 14:
+     case 15:
+     case 16:
+     case 17:
+     case 18:
+     case 19:
        return "You are a teenager"
     default:
        return "You have an age"
