7855e46bde0cd5d0c987119459662ba02d8d1526
Jake Rose <jake1020@mac.com>
2019-10-02 01:24:32 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,25 @@
-function basicTeenager(age) {
-
+function basicTeenager(age){
+if (age >= 13 && age =< 19) {
+  return ("You are a teenager!");
+}
 }
 
-function teenager(age) {
-
+function teenager(age){
+if (age >= 13 && age =< 19) {
+  return ("You are a teenager!");
+} else {
+  return ("You are not a teenager!");
+}
 }
 
 function ageChecker(age) {
-
+if (age >= 13 && age =< 19) {
+  return ("You are a teenager!");
+}else if {
+  return ();
+}else {
+  return ("You are not a teenager!");
+}
 }
 
 function ternaryTeenager(age) {
