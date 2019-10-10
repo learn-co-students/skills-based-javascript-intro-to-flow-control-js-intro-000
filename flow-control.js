d981b40b045bc57f3ffe4c69d7c5c27ff78e4355
Jean-François Rompré <jrompre@gmail.com>
2019-10-10 22:11:14 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,45 @@
 function basicTeenager(age) {
-
+if ((age <= 19) &&  (13 <= age)) {
+    return "You are a teenager!";
+    }
 }
 
 function teenager(age) {
-
+  if ((age <= 19) &&  (13 <= age)) {
+      return "You are a teenager!"
+    }
+  else {
+      return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if ((age <= 19) &&  (13 <= age)) {
+      return "You are a teenager!"
+    }
+  else if (age <= 12) {
+      return "You are a kid"
+    }
+  else if (age > 19) {
+      return "You are a grownup"
+    }
 }
 
 function ternaryTeenager(age) {
-
+   return "You are "
+        + (((age <= 19) &&  (13 <= age)) ? "a" : "not a")
+        + " teenager"
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19: return "You are a teenager"
+    default: return "You have an age"
+  }
 }
