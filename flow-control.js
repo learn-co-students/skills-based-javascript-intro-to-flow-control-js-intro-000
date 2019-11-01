8a2df33c3240e90b3dcafaec14ca733e1da128f9
Matthias Gerwin <matthiasjger@gmail.com>
2019-11-01 18:54:32 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -27,13 +27,19 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return (age ? 'You are a teenager' : 'You are not a teenager');
+  return (age >= 13 && age <= 19 ? 'You are a teenager' : 'You are not a teenager');
 
 }
 
 function switchAge(age) {
-  switch age {
-    case (age >= 13 && age <= 19):
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
     return "You are a teenager"
   default:
     return "You have an age"
