ac0cf98efe81ba2cfd604ffebc4d43723409f6c3
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-14 09:50:09 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -34,17 +34,11 @@ return (age >= 13 && age <= 19)? "You are a teenager" : "You are not a teenager"
 function switchAge(age) {
 switch(age) {
   case 13:
-  return "You are a teenager"
   case 14:
-  return "You are a teenager"
   case 15:
-  return "You are a teenager"
   case 16:
-  return "You are a teenager"
   case 17:
-  return "You are a teenager"
   case 18:
-  return "You are a teenager"
   case 19:
   return "You are a teenager"
   default:
