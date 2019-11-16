fc5932e3cf863913d6f8bfdd0266de77d0e3949c
Chandrashekar YM <shekar.ym@gmail.com>
2019-11-16 00:10:42 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,20 +1,20 @@
 function basicTeenager(age) {
-if (13<age<19){
-  return "You are a teenager!";
+if (age >= 13 && age <= 19) {
+  return 'You are a teenager!';
 }
 }
 function teenager(age) {
-if(13<age<19){
-  return("You are a teenager!");
+if(age >= 13 && age <= 19){
+  return 'You are a teenager!';
 }
 else {
-    return("You are not a teenager");
+    return 'You are not a teenager';
   }
 }
 
 function ageChecker(age) {
 
-if(13<age<19){
+if(age >=13 && age <= 19){
   return("You are a teenager!");
 } else if(age<=12){
   return("You are a kid");
@@ -26,7 +26,7 @@ else {
 
 function ternaryTeenager(age) {
 
-  return 13<age<19 ? "You are a teenager" : "You are not a teenager";
+  return age >=13 && age <=19 ? "You are a teenager" : "You are not a teenager";
 
 }
 
