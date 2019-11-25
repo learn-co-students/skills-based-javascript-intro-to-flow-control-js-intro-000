5a92610e634b8d2beb6d79e6fdd49ba2391df34f
Elizabeth T Hallett <ethallett@gmail.com>
2019-11-25 07:22:23 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,44 @@
 function basicTeenager(age) {
+  if(age > 12 && age < 20){
+    return("You are a teenager!");
+  }
 
 }
 
 function teenager(age) {
+  if(age > 12 && age < 20){
+    return("You are a teenager!");
+  } else {return("You are not a teenager")
+  }
 
 }
 
 function ageChecker(age) {
+  if(age > 12 && age < 20){
+    return("You are a teenager!");
+  } else if(age <= 12){
+    return("You are a kid");
+  } else {return("You are a grownup")}
 
 }
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager(age){
+  return (age > 12 && age < 20) ? "You are a teenager" :
+"You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch (age) {
+   case (age = 13):
+   case (age = 14):
+   case (age = 15):
+   case (age = 16):
+   case (age = 17):
+   case (age = 18):
+   case (age = 19):
+     return("You are a teenager");
+    break;
+   default:
+     return("You have an age");
+  }
 }
