79193c296968a3e0bcd0eae60f2c7015aec9db31
Edwin Meek <emeekiv@gmail.com>
2019-12-12 23:34:41 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,43 @@
 function basicTeenager(age) {
-
+if (age >= 13 && age <= 19){
+  return 'You are a teenager!'
+  }
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19){
+    return 'You are a teenager!'
+  }
+  else {
+    return 'You are not a teenager'
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19){
+    return 'You are a teenager!'
+  }
+  else if (age <13){
+    return 'You are a kid'
+  }
+  else if (age > 19){
+    return 'You are a grownup'
+  }
 }
 
 function ternaryTeenager(age) {
-
+return age >= 13 && age <= 19 ?
+'You are a teenager' :
+'You are not a teenager'
 }
 
 function switchAge(age) {
-
-}
+  switch (age){
+    case (age === 13 || age === 14 || age === 15 || age === 16 || age === 17 || age === 18 || age === 19):
+      return 'You are a teenager'
+      break;
+    default:
+      return 'You have an age'
+      break;
+    }
+  }
