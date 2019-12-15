8512c1422d2049b80977281738b52201a7179b7f
Edwin Meek <emeekiv@gmail.com>
2019-12-15 14:08:20 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -31,13 +31,17 @@ return age >= 13 && age <= 19 ?
 'You are not a teenager'
 }
 
-function switchAge(age) {
-  switch (age){
-    case (age === 13 || age === 14 || age === 15 || age === 16 || age === 17 || age === 18 || age === 19):
-      return 'You are a teenager'
-      break;
-    default:
-      return 'You have an age'
-      break;
-    }
+function switchAge(age){
+  switch (age) {
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+    return "You are a teenager";
+  default:
+    return "You have an age"
   }
+}
