0403188f448f3c9197570fe9e1e0633c21b406c4
Tim Kotowski <tkotowski7@gmail.com>
2020-01-13 20:02:22 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,44 @@
 function basicTeenager(age) {
-
+if (age === 13){
+  return "You are a teenager!"
+} else {
+  return undefined
+}
 }
 
 function teenager(age) {
-
+  if (age === 13){
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+if (age == 13){
+  return "You are a teenager!"
+}
+  if (age <= 12){
+  return 'You are a kid'
+}   else if (age >= 20){
+    return "You are a grownup"
+ }
 }
 
 function ternaryTeenager(age) {
-
+if (age === 15){
+  return "You are a teenager"
+} else if (age > 19){
+  return "You are not a teenager"
+}
 }
 
 function switchAge(age) {
-
+if (age >= 13 && age <= 19){
+  return 'You are a teenager'
+} if (age < 13){
+  return "You have an age"
+} else if (age > 19) {
+  return "You have an age"
+}
 }
