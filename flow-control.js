9310becb99d81920b5a2d0db4311eeb9eca0deeb
hilarious-tier-9201 <dhirahapani@hotmail.com>
2020-01-24 04:07:31 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,10 @@
 function basicTeenager(age) {
-
+  if (age === '13-19')
+  console.log(You are a teenager)
 }
-
 function teenager(age) {
-
+  if (age < 12 && age > 19)
+console.log(You are not a teenager)
 }
 
 function ageChecker(age) {
