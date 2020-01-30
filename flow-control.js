a765a8c799f45bebeb7616f250a28fbfc23ed597
zakir matin <zakir.matin@gmail.com>
2020-01-30 20:43:16 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,41 @@
-function basicTeenager(age) {
-
+function basicTeenager(age) { 
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"}
 }
 
-function teenager(age) {
 
+
+function teenager(age) { 
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }else { 
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >=13 && age <=19) {
+    return "You are a teenager!"
+  }else if (age > 19){
+    return "You are a grownup"
+  } else {
+    return "You are a kid"
+  }
 }
 
 function ternaryTeenager(age) {
-
+  if (age >= 13 && age <=19) {
+    return "You are a teenager"
+  }else {
+    return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
-
+  if (age >= 13 && age <=19) {
+    return "You are a teenager"
+  }else {
+    return "You have an age"
+  }
 }
+
