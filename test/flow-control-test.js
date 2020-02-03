accd7638d8ef471604761ec3863884e089f9ddbb
Avera Guthrie <averaguthrie@gmail.com>
2020-02-03 20:22:10 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -19,7 +19,7 @@ describe('flow-control', () => {
 
       return done()
     })
-  })
+  }) }
 
   function basicTeenager(age) {
     if(age >= 13 && age <= 19) {
@@ -54,25 +54,18 @@ describe('flow-control', () => {
     switch(age) {
       case '13':
         return 'You are a teenager'
-          break;
       case '14':
         return 'You are a teenager'
-          break;
       case '15':
         return 'You are a teenager'
-          break;
       case '16':
         return 'You are a teenager'
-          break;
       case '17':
         return 'You are a teenager'
-          break;
       case '18':
         return 'You are a teenager'
-          break;
       case '19':
         return 'You are a teenager'
-          break;
       default:
         return 'You have an age'
     }
