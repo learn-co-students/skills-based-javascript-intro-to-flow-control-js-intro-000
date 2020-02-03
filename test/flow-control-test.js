0a4aead312daf74b09eed25bcf33abc1ca2632a0
Avera Guthrie <averaguthrie@gmail.com>
2020-02-03 20:14:17 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -29,44 +29,51 @@ describe('flow-control', () => {
 
   function teenager(age) {
     if(age >= 13 && age <= 19) {
-      return 'You are a teenager!'
+      return 'You are a teenager!';
     } else {
-      return 'You are not a teenager'
+      return 'You are not a teenager';
     }
   }
 
-  describe('ageChecker', () => {
-    it('should return "You are a teenager!" if the age is between 13-19', () => {
-      expect(ageChecker(13)).toEqual("You are a teenager!")
-    })
+  function ageChecker(age) {
+    if(age >= 13 && age <= 19) {
+      return 'You are a teenager!';
+    } else if (age <= 12) {
+      return 'You are a kid'
+    } else {
+      return 'You are a grownup'
+    }
+  }
 
-    it('should return "You are a kid" if the age is 12 or below', () => {
-      expect(ageChecker(12)).toEqual("You are a kid")
-    })
 
-    it('should return "You are a grownup" if the age is 20 or above', () => {
-      expect(ageChecker(29)).toEqual("You are a grownup")
-    })
-  })
+  function ternaryTeenager(age) {
+  return age >= 13 && age <= 19 ? 'You are a teenager' : 'You are not a teenager'
+}
 
-  describe('ternaryTeenager', () => {
-    it('should return "You are a teenager" if age is between 13-19', () => {
-      expect(ternaryTeenager(15)).toEqual("You are a teenager")
-    })
-
-    it('should return "You are not a teenager" if age not between 13-19', () => {
-      expect(ternaryTeenager(75)).toEqual("You are not a teenager")
-    })
-  })
-
-  describe('switchAge', () => {
-    it('should return "You are a teenager" if age is between 13-19', () => {
-      expect(switchAge(15)).toEqual("You are a teenager")
-    })
-
-    it('should return "You have an age" if age not between 13-19', () => {
-      expect(switchAge(75)).toEqual("You have an age")
-      expect(switchAge(7)).toEqual("You have an age")
-    })
-  })
-})
+  function switchAge(age) {
+    switch(age) {
+      case '13':
+        return 'You are a teenager'
+          break;
+      case '14':
+        return 'You are a teenager'
+          break;
+      case '15':
+        return 'You are a teenager'
+          break;
+      case '16':
+        return 'You are a teenager'
+          break;
+      case '17':
+        return 'You are a teenager'
+          break;
+      case '18':
+        return 'You are a teenager'
+          break;
+      case '19':
+        return 'You are a teenager'
+          break;
+      default:
+        return 'You have an age'
+    }
+  }
