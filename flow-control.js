15fbc47c819f2cf85750907022194117172010c7
Esteban Flores <ebflores88@gmail.com>
2020-02-07 06:06:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -25,17 +25,39 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  if(age >= 13 && age <= 19){
+  /*if(age >= 13 && age <= 19){
     return "You are a teenager"
   } else {
     return "You are not a teenager"
-  }
+  }*/
+
+  return (age >= 13 && age <=19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-  if(age >= 13 && age <= 19){
+  /*
+    if(age >= 13 && age <= 19){
+      return "You are a teenager"
+    } else {
+      return "You have an age"
+    }
+  */
+  switch(age){
+  case 13:
     return "You are a teenager"
-  } else {
+  case 14:
+    return "You are a teenager"
+  case 15:
+    return "You are a teenager"
+  case 16:
+    return "You are a teenager"
+  case 17:
+    return "You are a teenager"
+  case 18:
+    return "You are a teenager"
+  case 19:
+    return "You are a teenager"
+  default:
     return "You have an age"
   }
 }
