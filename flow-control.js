01f0bf49253eba266893a4e60ee9e8d4fcc1ec2c
Antonia Gavrilis <antoniazabel@gmail.com>
2020-03-05 02:04:25 +0000

    Tests Passing

--- flow-control.js
+++ flow-control.js
@@ -20,5 +20,26 @@ function ageChecker(age) {
   }
 }
 function ternaryTeenager(age) {
-  return (age <= 12 && age <= 19) ? "You are a teenager" : "You are not a teenager"
+  return (age > 12 && age <= 19) ? "You are a teenager" : "You are not a teenager"
+}
+
+function switchAge(age) {
+  switch (age) {
+    case 13:
+      return "You are a teenager"
+    case 14:
+      return "You are a teenager"
+    case 15:
+      return "You are a teenager"
+    case 16:
+      return "You are a teenager"
+      case 17:
+        return "You are a teenager"
+        case 18:
+          return "You are a teenager"
+          case 19:
+            return "You are a teenager"
+        default:
+        return "You have an age"  // all other cases
+    }
 }
