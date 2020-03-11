b7c0af5cbc1d34137572b461a53f61fef4a8aea1
nick chen <nick.chen.pro@gmail.com>
2020-03-11 17:08:25 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -16,30 +16,31 @@ function basicTeenager(age) {
   }
 
 
-
-/* function ageChecker(age) {
+function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return 'You are a teenager!';
   } else if (age <= 12) {
     return 'You are a kid';
-  } else (age >= 20) {
+  } else if (age >= 20){
     return 'You are a grownup';
   }
-
-
 }
- function ternaryTeenager(age) {
-  return (age >= 13 && age <= 19) ?
-    'You are not a teenager' : 'You are a teenager';
-
+function ternaryTeenager(age) {
+  return (age >= 13 && age <= 19) ? 'You are a teenager' : 'You are not a teenager';
 }
 
  function switchAge(age) {
-  swtich(age) {
-    case (age >= 13 && age <= 19) :
+  switch (age) {
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
     return 'You are a teenager';
-    default:
+  default:
     return 'You have an age';
   }
 
-} */
+}
