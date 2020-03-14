073ab317203d28ebfb6e426ccc82687a40959938
moataz osama <moataz.osama@hotmail.com>
2020-03-14 16:07:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,57 @@
-function basicTeenager(age) {
-
+function basicTeenager (age)
+ {
+if (age >13 && age <19)
+{
+return "You are a teenager!";
 }
-
+}
 function teenager(age) {
-
-}
+    if (age >13 && age<19)
+  {return "you are a teenager!"}
+  else
+  { return "you are not a teenager"}
+  }
 
 function ageChecker(age) {
-
+  if (13<age<19)
+  {return "You are a teenager!"}
+  else if(age<=12)
+  {return "You are a kid"}
+  else if(age>19)
+  {    return "You are a grownup"}
 }
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager(age)
+{
+return 13<age<19? you are a teenager: You are not a teenager
 }
 
-function switchAge(age) {
+ function switchAge(age) {
+  switch(age)
+  {
+    case"13":
+    console.log("You are a teenager");
+    break;
+    case"14":
+    console.log("You are a teenager");
+    break;
+    case"15":
+    console.log("You are a teenager");
+    break;
+     case"16":
+    console.log("You are a teenager");
+    break;
+    case"17":
+    console.log("You are a teenager");
+    break;
+    case"18":
+    console.log("You are a teenager");
+    break;
+    case"19":
+    console.log("You are a teenager");
+    break;
+    default:
+    console.log("You have an age");
+  }
 
 }
