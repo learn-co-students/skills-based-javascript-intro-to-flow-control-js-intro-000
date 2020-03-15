ec92fd6298ac8b5c8e23a7a6b6128fd3fd86caaf
Tehuti English <ausareenglish@gmail.com>
2020-03-15 19:58:43 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,62 @@
 function basicTeenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
 
 }
 
 function teenager(age) {
+  if (age >= 13 && age <=19) {
+    return "You are a teenager!"
+  } else {
+     return "You are not a teenager"
+  }
 
 }
 
-function ageChecker(age) {
-
+function ageChecker(age) {       // determines your age group
+   if (age >= 13 && age <= 19) { // checks if you're a teenager
+    return "You are a teenager!" // confirms that you're a teenager
+  } else if (age <= 12) {        // checks if you're a kid      
+     return "You are a kid"       // confirms that you're a kind
+  } else {                        
+     return "You are a grownup"  // confirms that you are a grownup
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return (age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager");
 }
 
-function switchAge(age) {
+function switchAge(age) { 
+  switch(age) {           
+    case 13:              
+     return "You are a teenager";
+     break;
+    case 14:
+     return "You are a teenager";
+     break;
+    case 15:
+     return "You are a teenager";
+     break;
+    case 15:
+     return "You are a teenager";
+     break;
+    case 16:
+     return "You are a teenager";
+     break;
+    case 17:
+     return "You are a teenager";
+     break;
+    case 18:
+     return "You are a teenager";
+     break;
+    case 19:
+     return "You are a teenager";
+     break;
+    default:
+     return "You have an age";
+      
+  }
 
 }
