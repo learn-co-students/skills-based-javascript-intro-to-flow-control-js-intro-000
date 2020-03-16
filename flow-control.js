b8d8ef6213b28890389fc9e0000b0545c71c70b9
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-16 20:19:41 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -38,6 +38,12 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
 
+  if (age >= 13 && age <= 19)
+
+  {
+    return "You are a teenager!"
+  }
+  return "You are not a teenager!"
 }
 
 function switchAge(age) {
