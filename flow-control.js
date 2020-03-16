27480e85de886637dc119cf53a0a1f7f372e7572
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-16 20:36:58 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,7 @@ function teenager(age) {
     return "You are a teenager!"
   }
   else {
-    return "You are not a teenager!"
+    return "You are not a teenager"
   }
 }
 
@@ -41,11 +41,38 @@ function ternaryTeenager(age) {
   if (age >= 13 && age <= 19)
 
   {
-    return "You are a teenager!"
+    return "You are a teenager"
   }
-  return "You are not a teenager!"
+  return "You are not a teenager"
 }
 
 function switchAge(age) {
+
+  switch(age) {
+    case 13:
+      return "You are a teenager"
+      break;
+    case 14:
+      return "You are a teenager"
+      break;
+    case 15:
+      return "You are a teenager"
+      break;
+    case 16:
+      return "You are a teenager"
+      break;
+    case 17:
+      return "You are a teenager"
+      break;
+    case 18:
+      return "You are a teenager"
+      break;
+    case 19:
+      return "You are a teenager"
+      break;
+    default:
+      return "You have an age"
+      break;
+  }
 
 }
