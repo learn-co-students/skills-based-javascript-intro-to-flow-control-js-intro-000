caf20ea54be115628acfc5bc7620761e05296e04
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-03 21:06:50 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -26,7 +26,7 @@ describe('flow-control', () => {
       expect(basicTeenager(13)).toEqual("You are a teenager!");
     })
 
-    it('should return undefined if the age is not between 13-19', () => {
+    it('should return undefined if the age is not between 13-19',() => {
       expect(basicTeenager(12)).toBe(undefined)
     })
 
