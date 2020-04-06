c68bf8d9778ed71765cd5c11f7c7dcb67ebcd034
Amy Simpson <simpson.r.amy@gmail.com>
2020-04-06 18:10:29 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -32,56 +32,19 @@ function switchAge(age) {
   switch(age) {
     case 13:
       return "You are a teenager";
-      break;
     case 14:
       return "You are a teenager";
-      break;
     case 15:
       return "You are a teenager";
-      break;
     case 16:
       return "You are a teenager";
-      break;
     case 17:
       return "You are a teenager";
-      break;
     case 18:
       return "You are a teenager";
-      break;
     case 19:
       return "You are a teenager";
-      break;
     default:
       return "You have an age";
   }
 }
-
-/*
-function switchAge(age) {
-switch(age){
-case 13:
-return "You are a teenager";
-
-case 14:
-return "You are a teenager";
-
-case 15:
-return "You are a teenager";
-
-case 16:
-return "You are a teenager";
-
-case 17:
-return "You are a teenager";
-
-case 18:
-return "You are a teenager";
-
-case 19:
-return "You are a teenager";
-
-default:
-return "You have an age";
-}
-}
-*/
