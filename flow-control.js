b78a7a6b28d09b53ecce6ba43c5362854239691c
Joey Hendricks <joeysguitartabs@yahoo.com>
2020-04-09 11:02:42 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,29 @@
 function basicTeenager(age) {
-
+  if(age > 12 && age < 20) {
+    return 'You are a teenager!';
+  }
 }
+basicTeenager(15);
 
 function teenager(age) {
-
+  if(age > 12 && age < 20) {
+    return 'You are a teenager!';
+  } else {
+    return 'You are not a teenager';
+  }
 }
-
+teenager(17);
 function ageChecker(age) {
-
+  if(age > 12 && age < 20) {
+    return 'You are a teenager!';
+  } else if(age <= 12) {
+    return 'You are a kid';
+  } else {
+    return 'You are a grownup';
+  }
 }
-
+console.log(ageChecker(29));
+//console.log(ageTest);
 function ternaryTeenager(age) {
 
 }
