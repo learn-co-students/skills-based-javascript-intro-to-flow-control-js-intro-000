bc5da8dfa234e4249b380edbfe070f07e452efc5
KEITH ROGERS <asocialant@gmail.com>
2020-04-20 03:40:58 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,48 @@
-function basicTeenager(age) {
-
+function  basicTeenager (age){
+if ( age >12 || age <20){
+  console.log(`"You are a teenager!"`);
+}else {
+  console.log(0);
 }
+}
+
 
 function teenager(age) {
-
+if(age >12 || <20){
+  console.log(`"You are a teenager"`);
+}else {
+  console.log(`"you are not a teenager"`);
 }
+}
+teenager();
 
 function ageChecker(age) {
-
+  if (age >12 || <20){
+    console.log(`"you are a teenage"`);
+  }else if (age <=12) {
+    console.log(`"You are a kid"`);
+  }else if (age >=20) {
+console.log(`"You are a grownup"`);
+  }
 }
+ageChecker();
 
 function ternaryTeenager(age) {
-
+  if(age >12 ||<20){
+    Return(`You are a teenager`);
+  }else {
+    Return(`You are not a teenager`)
+  }
 }
-
+ternaryTeenager();
 function switchAge(age) {
+switch (switchage) {
+  case if(age >12 || <20):
+  Return(`You are a teenager`);
+
+    break;
+  default:Return(`You have an age`);
 
 }
+}
+ternaryTeenager()
