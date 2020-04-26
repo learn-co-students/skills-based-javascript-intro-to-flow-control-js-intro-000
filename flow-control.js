dfe382a17a1a352f109cebb75e24de257b718534
Anzel <anzelnieuwenhuizen@gmail.com>
2020-04-26 19:33:04 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -36,9 +36,8 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   if ( age >=13 && age<=19){
-    return ("You are a teenager!");
+    return ("You are a teenager");
   } else {
     return ("You have an age");
     }
   }
-}
