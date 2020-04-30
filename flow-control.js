cbaed8a3b061b2f8c2240af30e0eaf3369b5c239
Ruth Vera <rvamore89@gmail.com>
2020-04-30 20:46:45 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -20,40 +20,47 @@ function ageChecker(age) {
     return ("You are a kid")
 } else {
     return ("You are a grownup")
-}
+ }
 
 }
+
 
 function ternaryTeenager(age) {
   return (age>=13 && age<=19)? ("You are a teenager") : ("You are not a teenager")
 
 }
 
-function switchAge(age) {
-switch(age>=13 && age<=19) {
-  case (age=13):
-    return "You are a teenager"
-   break;
-  case (age=14):
-    return "You are a teenager"
-   break;
-  case (age=15):
-    return "You are a teenager"
-   break;
-  case (age=16):
-    return "You are a teenager"
-   break;
-  case (age=17):
-    return "You are a teenager"
-   break;
-  case (age=18):
-    return "You are a teenager"
-   break;
-  case (age=19):
-    return "You are a teenager"
-    break;
-  default:
-    return "You have an age"
- }
 
- }
+function switchAge(age) {
+  switch(age) {
+  case 13: (age>=13 && age<=19)
+      return "You are a teenager"
+  break;
+  case 14: (age>=13 && age<=19)
+      return "You are a teenager";
+  break;
+  case 15: (age>=13 && age<=19)
+      return "You are a teenager";
+  break;
+  case 16: (age>=13 && age<=19)
+        return "You are a teenager";
+  break;
+  case 17: (age>=13 && age<=19)
+        return "You are a teenager";
+  break;
+  case 18: (age>=13 && age<=19)
+        return "You are a teenager";
+  break;
+  case 19: (age>=13 && age<=19)
+        return "You are a teenager";
+  break;
+
+  default:
+
+  return "You have an age"
+
+
+
+}
+
+}
