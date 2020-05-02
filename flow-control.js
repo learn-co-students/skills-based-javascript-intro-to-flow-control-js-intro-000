53c1913e1ff040a8b6ad818f1b5939363f641b20
Laura Hatch <laura.hatch.lh@gmail.com>
2020-05-02 01:05:05 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,26 @@
+
 function basicTeenager(age) {
-if (>= 13 && <= 20) {
-return"You are a teenager!";
+if (age >= 13 && age <= 19) {
+return 'You are a teenager!';
   }
 }
 
 function teenager(age) {
-
-}
-
+if (age >= 13 && age <= 19){
+  return "You are a teenager!";
+} else{
+  return "You are not teenager";
+    }
+  }
+  
 function ageChecker(age) {
-
+if (age >= 13 && age <= 19){
+return "You are a teenager!";
+  }else if (age < 13){
+    return "You are a kid";
+  }else{
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
