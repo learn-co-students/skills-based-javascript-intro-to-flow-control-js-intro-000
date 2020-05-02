ebd917aa1738519df707cda776b449fd548b2f1a
Laura Hatch <laura.hatch.lh@gmail.com>
2020-05-02 02:05:53 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -31,19 +31,19 @@ if (age >= 13 && age <= 19){
   }
 }
 
-function switchAge(age) {
-switch(age)
-  case 13:
-  case 14:
-  case 15:
-  case 16:
-  case 17:
-  case 18:
-  case 19:
-    return "You are a teenager;
-  default:
-    return "You have an age"
-  }
-}
+function switchAge(age){
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return"You are a teenager";
+    default:
+      return "You have an age"
+    }
+ }
 
 
