45da44e5050afc567534aae5fc3eb15d923b470f
Harold Lee <harry.yg.lee@gmail.com>
2020-05-02 22:24:40 +0000

    Done.

---
+++ test/flow-control.js
@@ -0,0 +1,56 @@
+function basicTeenager(age) {
+  If(age >=13 && <=19)
+  {return "You are a teenager!";}
+}
+
+function teenager(age) {
+  If(age>=13 && <=19)
+  {return "You are a teenager!";}
+  else {
+  {return "You are not a teenager";}
+  }
+}
+
+function ageChecker(age) {
+  If(age>=13 && <=19)
+  {return "You are a teenager!";}
+  If(age<=12)
+  {return "You are a kid";}
+  If(age>=19)
+  {return "You are a grownup";}
+}
+
+function ternaryTeenager(age) {
+  if(age >=13 && age <=-19)
+  {return "You are a teenager";}
+  else
+  {return "You are not a teenager";}
+}
+
+function switchAge(age) {
+  case 13:
+  {return "You are a teenager";}
+
+  case 14:
+  {return "You are a teenager";}
+
+  case 15:
+  {return "You are a teenager";}
+
+  Case 16:
+  {return "You are a teenager";}
+
+  Case 17:
+  {return "You are a teenager";}
+
+  Case 18:
+  {return "You are a teenager";}
+
+  Case 19:
+  {return "You are a teenager";}
+
+  default:
+  {return "You have any age";}
+
+
+}
