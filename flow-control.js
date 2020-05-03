5cae522ca6f045563340427547aceb71c7e6a61b
Evaristo Sena <everett.sena79@gmail.com>
2020-05-03 11:50:25 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -14,8 +14,14 @@ else {return "You are a grownup"}
 function ternaryTeenager(age) {return (age >= 13 && age <= 19) ?
   "You are a teenager" : "You are not a teenager"}
 
-
-
-function switchAge(age) {
+function switchAge(age) {switch(age){
+case 13:
+case 14:
+case 15:
+case 16:
+case 17:
+case 18:
+case 19: return "You are a teenager";
+break; default: return "You have an age";}
 
 }
