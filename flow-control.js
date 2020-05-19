150cd2dbc89f7fef7a8f7e8d28f5d5d48b4148fc
Kaisha Smith <smith.kaisha04@gmail.com>
2020-05-19 04:33:00 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,59 @@
 function basicTeenager(age) {
-
+  if (13 <= age && age <= 19) {
+    return ("You are a teenager!");
 }
+}
+basicTeenager(15)
 
 function teenager(age) {
-
+  if (13 <= age && age <= 19) {
+    return ('You are a teenager!');}
+  else {return ('You are not a teenager');}
 }
+teenager(25)
 
 function ageChecker(age) {
+  if (13 <= age && age <= 19) {
+    return ('You are a teenager!');}
+  else if (age <= 12) {
+    return ('You are a kid');}
+  else {return("You are a grownup");}
 
 }
+
+ageChecker(25)
 
 function ternaryTeenager(age) {
-
+  return (13 <= age && age <= 19)?
+  "You are a teenager":"You are not a teenager"
 }
+ternaryTeenager(14)
 
 function switchAge(age) {
-
+  switch(age) {
+    case 13:
+      return('You are a teenager');
+      break;
+    case 14:
+      return('You are a teenager');
+      break;
+    case 15:
+      return('You are a teenager');
+      break;
+    case 16:
+      return('You are a teenager');
+      break;
+    case 17:
+      return('You are a teenager');
+      break;
+    case 18:
+      return('You are a teenager');
+      break;
+    case 19:
+    return('You are a teenager');
+    break;
+    default:
+    return "You have an age";
+  }
 }
+switchAge(25)
