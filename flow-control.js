5b4f938b07639484fcbbbc482792ce417f88c3c9
Callum Wilsmore <cal.wilsmore@btinternet.com>
2020-05-25 20:49:03 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,17 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) { return 'You are a teenager!'
+}
 }
 
 function teenager(age) {
-
+  if (age >= 13 && <= 19) { return "You are a teenager!" }
+  else { return "You are not a teenager" }
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) { return 'You are a teenager!' }
+  elseif (age <= 12) {return 'you are a kid'}
+  else (age >= 20 ) {return 'you are a grownup'}
 }
 
 function ternaryTeenager(age) {
