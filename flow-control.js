61c7758cd9b39b4e95ed1e2b960e181d7995791d
Waverley Leung <waverley.nyc@gmail.com>
2020-06-02 21:20:09 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return age < 19 && age > 13 ? "You are a teenager" : "You are not a teenager"
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
