29c00cd3d8f64fcf4fe97132f911fc593a77a1f8
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-24 02:46:47 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -28,19 +28,26 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  if (!(13 <= age) || !(age <= 19)){
-    return 'You are not a teenager'
-  }
-  else {
-    return 'You are a teenager'
-  }
+  return 13 <= age && age <= 19 ? 'You are a teenager' : 'You are not a teenager'
 }
 
 function switchAge(age) {
-  if (13 <= age && age <= 19){
-    return 'You are a teenager'
-  }
-  else {
-    return 'You have an age'
+  switch (age) {
+    case (13):
+    return "You are a teenager"
+    case (14):
+    return "You are a teenager"
+    case (15):
+    return "You are a teenager"
+    case (16):
+    return "You are a teenager"
+    case (17):
+    return "You are a teenager"
+    case (18):
+    return "You are a teenager"
+    case (19):
+    return "You are a teenager"
+    default:
+    return "You have an age"
   }
 }
