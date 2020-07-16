f7f7ff79361d49db2bea399a1fc9e6dae26975f9
Andrew Kennedy <akennedycode@gmail.com>
2020-07-16 19:51:48 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,47 @@
-function basicTeenager(age) {
-
+function basicTeenager(age){
+  if (age >= 13 && age <=19){
+    return "You are a teenager!";
+  }
 }
 
-function teenager(age) {
-
+function teenager(age){
+  if (age >= 13 && age <=19){
+    return "You are a teenager!";
+  }
+  else {
+    return "You are not a teenager";
+  }
 }
 
-function ageChecker(age) {
-
+function ageChecker(age){
+  if (age >= 13 && age <=19){
+    return "You are a teenager!";
+  }
+  else if (age < 13){
+    return "You are a kid";
+  }
+  else{
+    return "You are a grownup";
+  }
 }
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager(age){
+  return age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
 }
 
-function switchAge(age) {
+function switchAge(age){
+  switch (age) {
+  case 13:
+  case 14:
+   case 15:
+   case 16:
+   case 17:
+   case 18:
+   case 19:
+    return "You are a teenager";
+    break;
+  default:
+    return "You have an age"
+  }
 
 }
