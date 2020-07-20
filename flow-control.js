13ab5e8f930681feb0ef31beb9964f995bad01a8
Cole Cezeaux <colecezeaux@gmail.com>
2020-07-20 20:41:24 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,23 +1,51 @@
 function basicTeenager(age) {
-if (age >=19 && age <=13)
-  return "You are a teenager!"
+if (age <=19 && age >=13) {
+  return "You are a teenager!" }
 }
 
 function teenager(age) {
-  if (age >=19 && age <=13) {
+  if (age <=19 && age >=13) {
     return "You are a teenager!"
-  } else if (age <19 || age >13) {
-      return "You are not a teenager!" }
+  } else if (age >19 || age <13) {
+      return "You are not a teenager" }
 }
 
 function ageChecker(age) {
-
+  if (age <=19 && age >=13) {
+    return "You are a teenager!"
+  } else if (age >19) {
+    return "You are a grownup"
+  } else if (age <13) {
+    return "You are a kid" }
 }
 
 function ternaryTeenager(age) {
-
+return (age <=19 && age >=13) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+switch (age)
+age "13": {
+return ("You are a teenager"); }
+break;
+age "14": {
+return ("You are a teenager"); }
+break;
+age "15": {
+return ("You are a teenager"); }
+break;
+age "16": {
+return ("You are a teenager"); }
+break;
+age "17": {
+return ("You are a teenager"); }
+break;
+age "18": {
+return ("You are a teenager"); }
+break;
+age "19": {
+return ("You are a teenager"); }
+break;
+default: {
+return ("You have an age"); }
 }
