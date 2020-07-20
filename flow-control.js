8c71bd2129f6af9513d78cbec656c1841a522598
Cole Cezeaux <colecezeaux@gmail.com>
2020-07-20 19:01:49 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,14 @@
 function basicTeenager(age) {
-if (age >=19 && age <=13) {
-  return "You are a teenager!" }
+if (age >=19 && age <=13)
+  return "You are a teenager!"
 }
 
 function teenager(age) {
-  if (age >=19 && age <=13) {
-    return "You are a teenager!" }
+  if (age >=19 && age <=13)
+    return "You are a teenager!"
   } else {
-    if (age <19 || age >13) {
-      return "You are not a teenager!" }
+    if (age <19 || age >13)
+      return "You are not a teenager!"
 }
 
 function ageChecker(age) {
