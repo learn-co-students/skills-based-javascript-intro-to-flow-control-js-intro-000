fc52f3dd4a27a2ca0c4980dfe390a283c95dbf7b
Missouri Wilkinson <missourisw@gmail.com>
2020-07-22 16:31:16 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -28,9 +28,10 @@ return ( age>=13 &&  age<=19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-switch (age) {
+switch (true) {
   case (age>=13 && age<=19):
   return "You are a teenager";
   case (age<=12 || age>=20):
   return "You have an age";
+}
 }
