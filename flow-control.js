386ddfd03494bd21b5aae8eb0bfcf3c8e0c7cca4
Caitlin Oriel <croriel@gmail.com>
2020-07-29 14:46:55 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,33 @@
 function basicTeenager(age) {
-
+  if(age>=13&&<=19) {
+    return "You're a teenager!";
+  }
 }
 
 function teenager(age) {
-
+  if(age>=13&&<=19) {
+    return "You're a teenager!";
+  } else {
+    return "You're not a teenager";
+  }
 }
 
 function ageChecker(age) {
-
+  if(age>=13&&<=19) {
+    return "You're a teenager!";
+  } else if(age<=12) {
+    return "You're a kid";
+  } else(age>=19) {
+    return "You're a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return age>=13&&<=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case
+  }
 }
