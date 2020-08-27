f7b7ccfd7aa49422377aa1ef5f876e7df1827204
positive-proc-6135 <haydenmatz97@gmail.com>
2020-08-27 00:51:58 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -78,3 +78,42 @@ describe('flow-control', () => {
     })
   })
 })
+
+
+
+
+function basicTeenager(age) {
+  if (age<20 && age>12) {
+  return "You are a teenager!"
+  }
+}
+
+function teenager(age) {
+  if (age<20 && age>12) {
+    return "You are a teenager!"
+  }
+  return "You are not a teenager"
+}
+
+function ageChecker(age) {
+  if (age<20 && age>12) {
+    return "You are a teenager!"
+  } else if (age<13) {
+    return "You are a kid"
+  }
+  return "You are a grownup"
+}
+// else if (age>105) {return "You should be dead"}
+
+function ternaryTeenager(age) {
+return  (age<20 && age>12) ? "You are a teenager" : "You are not a teenager"
+}
+
+function switchAge(age) {
+  switch (true) {
+    case (age<20 && age>12):
+      return "You are a teenager"
+    default:
+      return "You have an age"
+  }
+}
