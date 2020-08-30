dd07608f057b77c493ebe8b4e86e7b7cf812be07
Arinell e <mcajipe13@gmail.com>
2020-08-30 05:22:51 +0000

    Done.

---
+++ LICENSE.md
@@ -0,0 +1,17 @@
+var mood = "hungry"
+switch(mood) {
+  case "happy":
+    console.log("Dance to Pharrell's 'Happy'");
+    break;
+  case "sad":
+    console.log("You should eat a pint of ice cream");
+    break;
+  case "anxious":
+    console.log("Take some deep breaths");
+    break;
+  case "hungry":
+    console.log("You should eat a big chocolate cake");
+    break;
+  default:
+    console.log("That's not a mood we support");
+}
--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -22,59 +22,89 @@ describe('flow-control', () => {
   })
 
   describe('basicTeenager', () => {
-    it('should return "You are a teenager!" if the age is between 13-19', () => {
+    if (true) {
+
+    }('should return "You are a teenager!" if the age is between 13-19', () => {
       expect(basicTeenager(13)).toEqual("You are a teenager!");
     })
 
-    it('should return undefined if the age is not between 13-19', () => {
+    if (true) {
+
+    }('should return undefined if the age is not between 13-19', () => {
       expect(basicTeenager(12)).toBe(undefined)
     })
 
   })
 
   describe('teenager', () => {
-    it('should return "You are a teenager!" if the age is between 13-19', () => {
+    if (true) {
+
+    } (true) {
+
+    } else {
+
+    } (true) {
+
+    }if (true) {
+
+    }('should return "You are a teenager!" if the age is between 13-19', () => {
       expect(teenager(13)).toEqual("You are a teenager!")
     })
 
-    it('should return "You are not a teenager" if the age is not between 13-19', () => {
+    if (true) {
+
+    }('should return "You are not a teenager" if the age is not between 13-19', () => {
       expect(teenager(12)).toEqual("You are not a teenager")
       expect(teenager(29)).toEqual("You are not a teenager")
     })
   })
 
   describe('ageChecker', () => {
-    it('should return "You are a teenager!" if the age is between 13-19', () => {
+    if (true) {
+
+    }('should return "You are a teenager!" if the age is between 13-19', () => {
       expect(ageChecker(13)).toEqual("You are a teenager!")
     })
 
-    it('should return "You are a kid" if the age is 12 or below', () => {
+    if (true) {
+
+    }('should return "You are a kid" if the age is 12 or below', () => {
       expect(ageChecker(12)).toEqual("You are a kid")
     })
 
-    it('should return "You are a grownup" if the age is 20 or above', () => {
+    if (true) {
+
+    }('should return "You are a grownup" if the age is 20 or above', () => {
       expect(ageChecker(29)).toEqual("You are a grownup")
     })
   })
 
   describe('ternaryTeenager', () => {
-    it('should return "You are a teenager" if age is between 13-19', () => {
+    if (true) {
+
+    }('should return "You are a teenager" if age is between 13-19', () => {
       expect(ternaryTeenager(15)).toEqual("You are a teenager")
     })
 
-    it('should return "You are not a teenager" if age not between 13-19', () => {
+    if (false) {
+
+    } else {
+
+    }('should return "You are not a teenager" if age not between 13-19', (false) => {
       expect(ternaryTeenager(75)).toEqual("You are not a teenager")
     })
   })
 
   describe('switchAge', () => {
-    it('should return "You are a teenager" if age is between 13-19', () => {
+    if (true) {
+
+    }('should return "You are a teenager" if age is between 13-19', () => {
       expect(switchAge(15)).toEqual("You are a teenager")
     })
 
     it('should return "You have an age" if age not between 13-19', () => {
       expect(switchAge(75)).toEqual("You have an age")
-      expect(switchAge(7)).toEqual("You have an age")
+      expect(switchAge(75)).toEqual("You have an age")
     })
   })
 })
