c9ed1faf0753a1421b8d33ebbc8d921be532219d
Amanda Silva <amanda.design.silva@gmail.com>
2020-09-03 15:22:52 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,37 @@
-function basicTeenager(age) {
-
+function basicTeenager( age ) {
+  if ( age == 13 >= 19 ) {
+    return "You are a teenager!"
+  } else if ( age >= 12 ) {
+    return "You are not a teenager"
+  }
 }
 
-function teenager(age) {
-
+function teenager( age ) {
+  if ( age == 13 >= 19 ) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
-function ageChecker(age) {
-
+function ageChecker( age ) {
+  if ( age == 13 >= 19 ) {
+    return "You are a teenager"
+  } else if ( age == >= 12 ) {
+    return "You are a kid"
+  } else if ( age >= 19 ) {
+    return "You are a growup"
+  }
 }
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager( age ) {
+  if ( age == 13 <= 19 ) {
+    return "You are a teenager"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
-function switchAge(age) {
+function switchAge( age ) {
 
 }
