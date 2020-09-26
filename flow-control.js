554605eb307d3f120799d1d9c945b184ab0e5f7a
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-26 14:01:09 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -26,10 +26,22 @@ return 13<=age && age<=19 ? "You are a teenager" : "You are not a teenager"
 
 function switchAge(age) {
   switch(age) {
-    case 13||14||15||16||17||18||19:
-      return('You are a teenager');
-      break;
+    case 13:
+    return 'You are a teenager';
+    case 14:
+    return 'You are a teenager';
+    case 15:
+    return 'You are a teenager';
+    case 16:
+    return 'You are a teenager';
+    case 17:
+    return 'You are a teenager';
+    case 18:
+    return 'You are a teenager';
+    case 19:
+    return 'You are a teenager';
+
     default:
-      return('You have an age');
-}
+      return 'You have an age';
+    }
 }
