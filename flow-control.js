5dc2f72b484080dc9c6e906c41872bae768efbe7
Caleb Roberts <calebtakespics@gmail.com>
2021-02-06 06:00:24 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,40 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!"
+  } else if (age <= 12){
+    return "You are a kid"
+  } else {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-
+  (age >= 13 && age <= 19) ? phrase = "You are a teenager": phrase = "You are not a teenager"
+  return phrase
 }
 
-function switchAge(age) {
-
-}
+//function switchAge(age) {
+  //let phrase = "";
+  //switch(age){
+  ///  case (age >= 13  age <= 19):
+    //  return = ("You are a teenager")
+      //break;
+  //  default:
+    //  return = ("You have an age")
+//  }
+  //return phrase
+//}
