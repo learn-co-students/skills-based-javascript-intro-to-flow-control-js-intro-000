f78dee935ff547029d317d21340ba066a3d3decc
Giovanni Rodriguez <ggabriel92@hotmail.com>
2021-03-08 03:28:23 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,38 @@
-function basicTeenager(age) {
+function basicTeenager(<=13 || >=19) {
+  console.log("you are a teenager")
+
 
 }
 
-function teenager(age) {
+function teenager("You're a teenager!") {
+  if (teenager <=13)
+  else ("You're not a teenager")
+
 
 }
 
-function ageChecker(age) {
+function ageChecker("You're a teenager") {
+  if (ageChecker (=>13)) || (ageChecker (<=19))
+  else if (ageChecker (<13))
+    console.log("You're a kid")
+  else if (ageChecker (>19))
+    console.log("You're a grown up")
 
 }
 
-function ternaryTeenager(age) {
+function ternaryTeenager("You're a teenager") {
+  if (ternaryTeenager (13, 14, 15, 16, 17, 18, 19))
+    console.log("You're a teenager")
+  break;
+  else if (ternaryTeenager (<13 || >19))
+  return ("You're not a teenager")
 
 }
 
-function switchAge(age) {
+function switchAge("You're a teenager") {
+  if (switchAge (=> 13)) || (switchAge) (<= 19))
+    console.log("You're a teenager")
+  else (switchAge (< 13 || >19))
+    console.log("You have an age")
 
 }
