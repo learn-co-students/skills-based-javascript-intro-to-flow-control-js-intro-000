054d6676b55ef513ba2ab8b89fc359837f5d7163
Christina Petersen <christina.h.petersen4@gmail.com>
2021-03-18 14:35:58 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,10 @@
 function basicTeenager(age) {
-
-}
+  if (age >= 13 && age <= 19) {
+    return ('You are a teenager!')
+  } else {
+    return ('You are not a teenager')
+  }
+};
 
 function teenager(age) {
 
