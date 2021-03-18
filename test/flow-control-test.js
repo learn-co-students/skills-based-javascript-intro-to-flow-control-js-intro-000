83ef503cfc9fb239c86a405a760c89d7ab7f6f78
Priyadharshini Thirukonda Mohanlal <priyamohanlal23@gmail.com>
2021-03-18 00:07:22 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -69,7 +69,7 @@ describe('flow-control', () => {
 
   describe('switchAge', () => {
     it('should return "You are a teenager" if age is between 13-19', () => {
-      expect(switchAge(15)).toEqual("You are a teenager")
+      expect(switchAge(13)).toEqual("You are a teenager")
     })
 
     it('should return "You have an age" if age not between 13-19', () => {
