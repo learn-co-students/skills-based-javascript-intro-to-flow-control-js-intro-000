edc61411d38798ccec882c8c35d2ee65c6d29925
Anri Kawahara <anri0806@hotmail.co.jp>
2021-03-26 20:17:38 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,38 @@
 function basicTeenager(age) {
-
+if (13<=19) {
+  return "You are a teenager!"
+}
 }
 
 function teenager(age) {
-
+if (13<=19) {
+  return "You are a teenager!";
+} else {
+    return "You are not a teenager";
+}
 }
 
 function ageChecker(age) {
-
+if (13<=19) {
+  return "You are a teenager!";
+} else if (0<=12) {
+  return "You are a kid";
+}else{
+  return "You are a grownup";
+}
 }
 
 function ternaryTeenager(age) {
-
+return 13<=19 ? "You are a teenager" :
+"You are not a teenager"
 }
 
 function switchAge(age) {
-
+switch(age) {
+    case 13<=19:
+     return "You are a teenager";
+    break;
+    default:
+     return "You have an age";
+  }
 }
