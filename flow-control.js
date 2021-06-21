ae56cdbddd89c368100a17921d96ed75f2b0de9c
Yearim Yi <leslieyi411@gmail.com>
2021-06-21 20:33:31 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -36,22 +36,17 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch (age) {
-    case 13:
-      return "You are a teenager"
+   switch (age) { 
+     case 13:
     case 14:
-      return "You are a teenager"
     case 15:
-      return "You are a teenager"
     case 16:
-      return "You are a teenager"
     case 17:
-      return "You are a teenager"
     case 18:
-      return "You are a teenager"
     case 19:
       return "You are a teenager"
-  default:
+      break;
+    default:
       return "You have an age"
   }
   
